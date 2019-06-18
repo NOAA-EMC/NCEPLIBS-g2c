ec9d7dcef4f4328446bc12f0d374c76125f2da0a
Dexin.Zhang <dexin.zhang@noaa.gov>
2019-06-18 15:20:22 +0000

    Unified build (20190618) script/makefile g2c

--- Conf/G2c_intel_General.sh
+++ Conf/G2c_intel_General.sh
@@ -18,15 +18,26 @@
  export G2C_SRC=
  export G2C_LIB4=$ANCHORDIR/libg2c_${G2C_VER}_4.a
 
-#JPZlib=/nwprod2/lib                                #WCOSS
- JPZlib=/scratch3/NCEPDEV/nwprod/lib                #THEIA
- export JASPER_VER=v1.900.1
- export JASPER_INC=$JPZlib/jasper/v1.900.1/include
- export PNG_VER=v1.2.44
-#export PNG_INC=$JPZlib/png/v1.2.44/include         #WCOSS
- export PNG_INC=$JPZlib/png/v1.2.44/src/include     #THEIA
- export Z_VER=v1.2.6
- export Z_INC=$JPZlib/z/v1.2.6/include
+#  JASPER, PNG and Z LIB in default system include *
+#  *** set nothing ***
+
+#  WCOSS WCOSS WCOSS WCOSS WCOSS WCOSS WCOSS WCOSS *
+#JPZlib=/nwprod2/lib
+#JASPER_VER=v1.900.1
+#PNG_VER=v1.2.44
+#Z_VER=v1.2.6
+#JASPER_INC=$JPZlib/jasper/v1.900.1/include
+#PNG_INC=$JPZlib/png/v1.2.44/include
+#Z_INC=$JPZlib/z/v1.2.6/include
+
+#  THEIA THEIA THEIA THEIA THEIA THEIA THEIA THEIA *
+#JPZlib=/scratch3/NCEPDEV/nwprod/lib
+#JASPER_VER=v1.900.1
+#PNG_VER=v1.2.44
+#Z_VER=v1.2.6
+#JASPER_INC=$JPZlib/jasper/v1.900.1/include
+#PNG_INC=$JPZlib/png/v1.2.44/src/include
+#Z_INC=$JPZlib/z/v1.2.6/include
 
  export CC=icc
  export FC=ifort
@@ -50,9 +61,12 @@
  export I8R8="-integer-size 64 -real-size 64"
 
  export CPPDEFS=""
- export CFLAGSDEFS="-I${PNG_INC} -I${JASPER_INC} -I${Z_INC} -DUNDERSCORE -DLINUX -DUSE_JPEG2000 -DUSE_PNG -D__64BIT__"
+ [[ -z "${PNG_INC-}" ]] && { IPNG= ; } || { IPNG=-I$PNG_INC; }
+ [[ -z "${JASPER_INC-}" ]] && { IJASPER= ; } || { IJASPER=-I$JASPER_INC; }
+ [[ -z "${Z_INC-}" ]] && { IZ= ; } || { IZ=-I$Z_INC; }
+ export CFLAGSDEFS="$IPNG $IJASPER $IZ -DUNDERSCORE -DLINUX -DUSE_JPEG2000 -DUSE_PNG -D__64BIT__"
  export FFLAGSDEFS=""
 
  export USECC="YES"
  export USEFC=""
- export DEPS="JASPER $JASPER_VER, LIBPNG $PNG_VER, ZLIB $Z_VER"
+ export DEPS="JASPER ${JASPER_VER-}, LIBPNG ${PNG_VER-}, ZLIB ${Z_VER-}"
--- build_g2c.sh
+++ build_g2c.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 
- (( $# == 0 )) && {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
-
- sys=${1,,}
- [[ $sys == wcoss || $sys == dell || $sys == cray ||\
-    $sys == theia || $sys == intel_general || $sys == gnu_general ]] || {
-   echo "*** Usage: $0 wcoss|dell|cray|theia|intel_general|gnu_general [debug|build] [[local]install[only]]" >&2
-   exit 1
- }
- debg=false
- inst=false
- skip=false
- local=false
- (( $# > 1 )) && {
-   [[ ${2,,} == build ]] && debg=false
-   [[ ${2,,} == debug ]] && debg=true
-   [[ ${2,,} == install ]] && inst=true
-   [[ ${2,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${2,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${2,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
- (( $# > 2 )) && {
-   [[ ${3,,} == build ]] && debg=false
-   [[ ${3,,} == debug ]] && debg=true
-   [[ ${3,,} == install ]] && inst=true
-   [[ ${3,,} == localinstall ]] && { local=true; inst=true; }
-   [[ ${3,,} == installonly ]] && { inst=true; skip=true; }
-   [[ ${3,,} == localinstallonly ]] && { local=true; inst=true; skip=true; }
- }
-
+ source ./Conf/Analyse_args.sh
  source ./Conf/Collect_info.sh
  source ./Conf/Gen_cfunction.sh
  source ./Conf/Reset_version.sh
@@ -81,14 +50,21 @@ set -x
 #     Install libraries and source files
 #
    $local && {
-              LIB_DIR4=..
-              SRC_DIR=
-             } || {
-              LIB_DIR4=$(dirname ${G2C_LIB4})
-              SRC_DIR=$G2C_SRC
-              [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
-              [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
-             }
+     instloc=..
+     LIB_DIR4=$instloc
+     SRC_DIR=
+   } || {
+     [[ $instloc == --- ]] && {
+       LIB_DIR4=$(dirname ${G2C_LIB4})
+       SRC_DIR=$G2C_SRC
+     } || {
+       LIB_DIR4=$instloc
+       SRC_DIR=$instloc/src
+       [[ $instloc == .. ]] && SRC_DIR=
+     }
+     [ -d $LIB_DIR4 ] || mkdir -p $LIB_DIR4
+     [ -z $SRC_DIR ] || { [ -d $SRC_DIR ] || mkdir -p $SRC_DIR; }
+   }
 
    make clean LIB=
    make install LIB=$g2cLib4 LIB_DIR=$LIB_DIR4 SRC_DIR=$SRC_DIR
