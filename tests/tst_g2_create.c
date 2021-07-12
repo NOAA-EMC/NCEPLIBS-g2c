a746e6ece5fc0609e1c81ac3766e5d63fdceba95
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-07-12 01:45:07 -0600

    adding test

--- tests/tst_g2_create.c
+++ tests/tst_g2_create.c
@@ -5,11 +5,23 @@
  */
 
 #include <stdio.h>
+#include "grib2.h"
+
+#define MSG_LEN 25
+#define G2C_ERROR 2
 
 int
 main()
 {
     printf("Testing g2_create().\n");
+    {
+	unsigned char cgrib[MSG_LEN];
+	g2int listsec0[2] = {0, 2};
+	g2int listsec1[13] = {0, 0, 0, 0, 0, 2021, 9, 22, 0, 0, 0, 0, 0};
+    
+	if (g2_create(cgrib, listsec0, listsec1))
+	    return G2C_ERROR;
+    }
     printf("SUCCESS!\n");
     return 0;
 }
