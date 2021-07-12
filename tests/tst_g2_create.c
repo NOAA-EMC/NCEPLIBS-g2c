5fc1fb900f2973f0b84910620623dcc51bca6348
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-07-11 18:42:21 -0600

    adding first test

---
+++ tests/tst_g2_create.c
@@ -0,0 +1,15 @@
+/* This is a test for the NCEPLIBS-g2c project. This test is for
+ * g2_create.
+ *
+ * Ed Hartnett 7/11/21
+ */
+
+#include <stdio.h>
+
+int
+main()
+{
+    printf("Testing g2_create().\n");
+    printf("SUCCESS!\n");
+    return 0;
+}
