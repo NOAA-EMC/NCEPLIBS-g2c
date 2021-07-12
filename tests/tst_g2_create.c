16d60d9afb7fd75f5e6fd2a34e20d0f6cda35548
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-07-12 01:55:16 -0600

    basic test

--- tests/tst_g2_create.c
+++ tests/tst_g2_create.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include "grib2.h"
 
-#define MSG_LEN 25
+#define MSG_LEN 37
 #define G2C_ERROR 2
 
 int
@@ -16,11 +16,24 @@ main()
     printf("Testing g2_create().\n");
     {
 	unsigned char cgrib[MSG_LEN];
+	unsigned char expected_cgrib[MSG_LEN] = {71, 82, 73, 66, 0, 0, 0, 2, 0, 0,
+						 0, 0, 0, 0, 0, 37, 0, 0, 0, 21, 1,
+						 0, 0, 0, 0, 0, 0, 0, 7, 229, 9, 22,
+						 0, 0, 0, 0, 0};
 	g2int listsec0[2] = {0, 2};
 	g2int listsec1[13] = {0, 0, 0, 0, 0, 2021, 9, 22, 0, 0, 0, 0, 0};
+	
+	int i;
+	int ret;
     
-	if (g2_create(cgrib, listsec0, listsec1))
+	if ((ret = g2_create(cgrib, listsec0, listsec1)) != MSG_LEN)
 	    return G2C_ERROR;
+	for (i = 0; i < MSG_LEN; i++)
+	{
+	    /* printf("%d %d %d\n", i, cgrib[i], expected_cgrib[i]); */
+	    if (cgrib[i] != expected_cgrib[i])
+	    	return G2C_ERROR;
+	}
     }
     printf("SUCCESS!\n");
     return 0;
