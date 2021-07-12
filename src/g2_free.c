651b7d0fb6b138538d5afb755273fe1ed1a11dba
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-07-12 02:12:19 -0600

    more cleanup

--- src/g2_free.c
+++ src/g2_free.c
@@ -20,7 +20,8 @@
  *
  * @author Gilbert ORG: W/NP11 @date 2002-10-28
  */
-void g2_free(gribfield *gfld)
+void
+g2_free(gribfield *gfld)
 {
     if (gfld->idsect != 0 ) free(gfld->idsect);
     if (gfld->local != 0 ) free(gfld->local);
