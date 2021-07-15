21c8d2cd40858d95e7a0f1d4374e6389d5537f32
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-07-15 08:43:27 -0600

    fixed g2_unpack6.c

--- src/g2_unpack6.c
+++ src/g2_unpack6.c
@@ -32,55 +32,55 @@
  */
 g2int
 g2_unpack6(unsigned char *cgrib, g2int *iofst, g2int ngpts, g2int *ibmap,
-               g2int **bmap)
+           g2int **bmap)
 {
-      g2int j,ierr,isecnum;
-      g2int *lbmap=0;
-      g2int *intbmap;
+    g2int j,ierr,isecnum;
+    g2int *lbmap=0;
+    g2int *intbmap;
 
-      ierr=0;
-      *bmap=0;    /* NULL */
+    ierr=0;
+    *bmap=0;    /* NULL */
 
-      *iofst=*iofst+32;    /* skip Length of Section */
-      gbit(cgrib,&isecnum,*iofst,8);         /* Get Section Number */
-      *iofst=*iofst+8; 
+    *iofst=*iofst+32;    /* skip Length of Section */
+    gbit(cgrib,&isecnum,*iofst,8);         /* Get Section Number */
+    *iofst=*iofst+8;
 
-      if ( isecnum != 6 ) {
-         ierr=2;
-         fprintf(stderr,"g2_unpack6: Not Section 6 data.\n");
-         return(ierr);
-      }
+    if ( isecnum != 6 ) {
+        ierr=2;
+        fprintf(stderr,"g2_unpack6: Not Section 6 data.\n");
+        return(ierr);
+    }
 
-      gbit(cgrib,ibmap,*iofst,8);    /* Get bit-map indicator */
-      *iofst=*iofst+8;
+    gbit(cgrib,ibmap,*iofst,8);    /* Get bit-map indicator */
+    *iofst=*iofst+8;
 
-      if (*ibmap == 0) {               /* Unpack bitmap */
-         if (ngpts > 0) lbmap=(g2int *)calloc(ngpts,sizeof(g2int));
-         if (lbmap == 0) {
+    if (*ibmap == 0) {               /* Unpack bitmap */
+        if (ngpts > 0) lbmap=(g2int *)calloc(ngpts,sizeof(g2int));
+        if (lbmap == 0) {
             ierr=6;
             return(ierr);
-         }
-         else {
+        }
+        else {
             *bmap=lbmap;
-         }
-         intbmap=(g2int *)calloc(ngpts,sizeof(g2int));  
-         gbits(cgrib,intbmap,*iofst,1,0,ngpts);
-         *iofst=*iofst+ngpts;
-         for (j=0;j<ngpts;j++) {
-           lbmap[j]=(g2int)intbmap[j];
-         }
-         free(intbmap);
-	 /*
-	   else if (*ibmap.eq.254)               ! Use previous bitmap
-	   return(ierr);
-	   else if (*ibmap.eq.255)               ! No bitmap in message
-	   bmap(1:ngpts)=.true.
-	   else {
-	   print *,'gf_unpack6: Predefined bitmap ',*ibmap,' not recognized.'
-	   ierr=4;
-	 */
-      }
-      
-      return(ierr);    /* End of Section 6 processing */
+        }
+        intbmap=(g2int *)calloc(ngpts,sizeof(g2int));
+        gbits(cgrib,intbmap,*iofst,1,0,ngpts);
+        *iofst=*iofst+ngpts;
+        for (j=0;j<ngpts;j++) {
+            lbmap[j]=(g2int)intbmap[j];
+        }
+        free(intbmap);
+        /*
+          else if (*ibmap.eq.254)               ! Use previous bitmap
+          return(ierr);
+          else if (*ibmap.eq.255)               ! No bitmap in message
+          bmap(1:ngpts)=.true.
+          else {
+          print *,'gf_unpack6: Predefined bitmap ',*ibmap,' not recognized.'
+          ierr=4;
+        */
+    }
+
+    return(ierr);    /* End of Section 6 processing */
 
 }
