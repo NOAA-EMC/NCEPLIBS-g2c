d869de63e502bc1475d719745e950b38d96806ce
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-08-05 08:53:42 -0600

    updated version number in two places

--- src/grib2.h
+++ src/grib2.h
@@ -152,7 +152,7 @@
 #define _grib2_H
 #include <stdio.h>
 
-#define G2_VERSION "g2clib-1.6.2" /**< Current version of NCEPLIBS-g2c library. */
+#define G2_VERSION "g2clib-1.6.4" /**< Current version of NCEPLIBS-g2c library. */
 
 #ifdef __64BIT__
 typedef int g2int; /**< Integer type. */
