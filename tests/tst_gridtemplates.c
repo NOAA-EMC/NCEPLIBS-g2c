23d823bc2c235c37bffb00f81cde2d1175dcc6a3
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-24 09:11:36 -0600

    more test development

--- tests/tst_gridtemplates.c
+++ tests/tst_gridtemplates.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "grib2.h"
 
 #define G2C_ERROR 2
@@ -33,17 +34,33 @@ main()
             return G2C_ERROR;
     }
     printf("ok!\n");
-    printf("Testing simple getgridtemplate() calls...");
+    printf("Testing simple getgridtemplate() calls (expect and ignore error messages)...");
     {
         /* Note that gtemplate is not the same as gribtemplate. */
         gtemplate *tmpl;
-
-        /* Check for one that's there. */
+        g2int list[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        
+        /* Check for one that's there and does not need extension. */
         if (!(tmpl = getgridtemplate(1)))
             return G2C_ERROR;
         if (tmpl->type != 3 || tmpl->num != 1 || tmpl->maplen != 22 || tmpl->needext ||
             tmpl->extlen || tmpl->ext)
             return G2C_ERROR;
+        free(tmpl);
+
+        /* Check for one that's there and does need extension. */
+        if (!(tmpl = getgridtemplate(4)))
+            return G2C_ERROR;
+        if (tmpl->type != 3 || tmpl->num != 4 || tmpl->maplen != 13 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl);
+
+        /* Check for one that's there and does need extension. */
+        if (!(tmpl = extgridtemplate(4, list)))
+            return G2C_ERROR;
+        if (tmpl->type != 3 || tmpl->num != 4 || tmpl->maplen != 13 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
         free(tmpl);
 
         /* Check for one that's not there. */
