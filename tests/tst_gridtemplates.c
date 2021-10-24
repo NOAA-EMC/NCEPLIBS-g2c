355eb6558e9dca003e565907f2a24adf584c4fdd
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-24 07:51:19 -0600

    adding test for gridtemplates

---
+++ tests/tst_gridtemplates.c
@@ -0,0 +1,23 @@
+/* This is a test for the NCEPLIBS-g2c project. This test is for
+ * gridtemplates.c.
+ *
+ * Ed Hartnett 10/24/21
+ */
+
+#include <stdio.h>
+#include "grib2.h"
+
+#define G2C_ERROR 2
+
+int
+main()
+{
+    
+    printf("Testing gridtemplates.\n");
+    printf("Testing simple getgridtemplate() call...");
+    {
+    }
+    printf("ok!\n");
+    printf("SUCCESS!\n");
+    return 0;
+}
