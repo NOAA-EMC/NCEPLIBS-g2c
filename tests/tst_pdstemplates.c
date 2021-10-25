bed3f554ad84a4ece92a36946aefa6f7be58902e
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-25 06:31:28 -0600

    more testing

--- tests/tst_pdstemplates.c
+++ tests/tst_pdstemplates.c
@@ -83,6 +83,221 @@ main()
         free(tmpl->ext);
         free(tmpl);
 
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(4, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 4 || tmpl->maplen != 30 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(8, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 8 || tmpl->maplen != 29 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(9, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 9 || tmpl->maplen != 36 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(10, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 10 || tmpl->maplen != 30 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(11, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 11 || tmpl->maplen != 32 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(12, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 12 || tmpl->maplen != 31 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(13, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 13 || tmpl->maplen != 45 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(14, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 14 || tmpl->maplen != 44 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(30, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 30 || tmpl->maplen != 5 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(31, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 31 || tmpl->maplen != 5 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(42, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 42 || tmpl->maplen != 30 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(43, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 43 || tmpl->maplen != 33 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(32, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 32 || tmpl->maplen != 10 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(46, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 46 || tmpl->maplen != 35 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(47, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 47 || tmpl->maplen != 38 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(51, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 51 || tmpl->maplen != 16 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(91, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 91 || tmpl->maplen != 36 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(33, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 33 || tmpl->maplen != 18 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(34, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 34 || tmpl->maplen != 32 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(53, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 53 || tmpl->maplen != 19 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(54, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 54 || tmpl->maplen != 22 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(57, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 57 || tmpl->maplen != 7 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(61, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 61 || tmpl->maplen != 38 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
+
+        /* Check for one that's there but does need an extension. */
+        tmpl = extpdstemplate(35, list);
+        if (!tmpl)
+            return G2C_ERROR;
+        if (tmpl->num != 35 || tmpl->maplen != 6 || !tmpl->needext)
+            return G2C_ERROR;
+        free(tmpl->ext);
+        free(tmpl);
     }
     printf("SUCCESS!\n");
     printf("ok!\n");
