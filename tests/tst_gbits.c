faebb3ef8bae5dc2e37845faf095c03383ef4799
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-25 11:29:03 -0600

    more testing

--- tests/tst_gbits.c
+++ tests/tst_gbits.c
@@ -88,22 +88,39 @@ main()
                 return G2C_ERROR;
     }
     printf("ok!\n");
-    /* printf("Testing more sbit() calls..."); */
-    /* { */
-    /* 	unsigned char out; */
-    /* 	g2int in = 1; */
-    /* 	g2int expected_out[8] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01}; */
-    /* 	int i; */
+    printf("Testing more sbit() calls...");
+    {
+	unsigned char out;
+	g2int in = 1;
+	g2int expected_out[8] = {0x95, 0x55, 0x35, 0x15, 0x0d, 0x05, 0x03, 0x01};
+	int i;
 
-    /* 	for (i = 0; i < 8; i++) */
-    /* 	{ */
-    /* 	    sbit(&out, &in, 0, i + 1); */
-    /* 	    /\* printf("in 0x%02x out 0x%02x\n", in, out); *\/ */
-    /* 	    if (out != expected_out[i]) */
-    /* 		return G2C_ERROR; */
-    /* 	} */
-    /* } */
-    /* printf("ok!\n"); */
+	for (i = 0; i < 8; i++)
+	{
+	    sbit(&out, &in, 0, i + 1);
+	    /* printf("0x%02x, ", out); */
+	    if (out != expected_out[i])
+	        return G2C_ERROR;
+	}
+    }
+    printf("ok!\n");
+    printf("Testing gbit() call...");
+    {
+#define MSG_LEN 109
+        g2int lencurr;
+        /* This message comes from the code in tst_g2_addgrid.c. */
+        unsigned char cgrib[MSG_LEN] = {71, 82, 73, 66, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0,
+            109, 0, 0, 0, 21, 1, 0, 7, 0, 4, 24, 0, 0, 7, 229, 10, 24, 6, 54, 59, 7, 192, 0, 0, 0,
+            72, 3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 3, 0, 0, 0, 4, 5, 0, 0, 0, 6, 0, 0,
+            0, 7, 0, 0, 0, 8, 0, 0, 0, 9, 0, 0, 0, 10, 0, 0, 0, 11, 0, 0, 0, 12, 13, 0, 0, 0, 14,
+            0, 0, 0, 15, 0, 0, 0, 16, 0, 0, 0, 17, 18};
+
+        gbit(cgrib, &lencurr, 96, 32);
+        if (lencurr != MSG_LEN)
+            return G2C_ERROR;
+
+    }
+    printf("ok!\n");
     printf("SUCCESS!\n");
     return 0;
 }
