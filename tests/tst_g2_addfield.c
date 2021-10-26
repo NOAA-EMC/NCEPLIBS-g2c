4bbf9d28ad391d4aa6b24937caab6f482e11a26a
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-26 05:17:24 -0600

    check full message

--- tests/tst_g2_addfield.c
+++ tests/tst_g2_addfield.c
@@ -26,26 +26,14 @@ main()
         g2int listsec1[13] = {7, 4, 24, 0, 0, 2021, 10, 24, 6, 54, 59, 7, 192};
         g2int igds[5] = {0, 4, 0, 0, 0};
         g2int igdstmpl[19] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
-        unsigned char expected_cgrib[MSG_LEN] = {71, 82, 73, 66, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0,
-            109, 0, 0, 0, 21, 1, 0, 7, 0, 4, 24, 0, 0, 7, 229, 10, 24, 6, 54, 59, 7, 192, 0, 0, 0,
-            72, 3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 3, 0, 0, 0, 4, 5, 0, 0, 0, 6, 0, 0,
-            0, 7, 0, 0, 0, 8, 0, 0, 0, 9, 0, 0, 0, 10, 0, 0, 0, 11, 0, 0, 0, 12, 13, 0, 0, 0, 14,
-            0, 0, 0, 15, 0, 0, 0, 16, 0, 0, 0, 17, 18};
-        int i;
-        int ret;
-
-        if ((ret = g2_create(cgrib, listsec0, listsec1)) != SEC0_LEN + SEC1_LEN)
-            return G2C_ERROR;
-
-        if ((ret = g2_addgrid(cgrib, igds, igdstmpl, NULL, 0)) != 109)
-            return G2C_ERROR;
-        for (i = 0; i < MSG_LEN; i++)
-        {
-            /* printf("%d %d %d\n", i, cgrib[i], expected_cgrib[i]);  */
-            if (cgrib[i] != expected_cgrib[i])
-                return G2C_ERROR;
-        }
-
+        unsigned char expected_cgrib[FULL_MSG_LEN] = {71, 82, 73, 66, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0,
+            182, 0, 0, 0, 21, 1, 0, 7, 0, 4, 24, 0, 0, 7, 229, 10, 24, 6, 54, 59, 7, 192, 0, 0, 0,
+            72, 3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 3, 0, 0, 0, 4, 5, 0, 0, 0, 6, 0,
+            0, 0, 7, 0, 0, 0, 8, 0, 0, 0, 9, 0, 0, 0, 10, 0, 0, 0, 11, 0, 0, 0, 12, 13, 0, 0, 0,
+            14, 0, 0, 0, 15, 0, 0, 0, 16, 0, 0, 0, 17, 18, 0, 0, 0, 34, 4, 0, 0, 0, 0, 0, 1, 2,
+            3, 4, 0, 5, 6, 7, 0, 0, 0, 8, 9, 10, 0, 0, 0, 11, 12, 13, 0, 0, 0, 14, 0, 0, 0, 21,
+            5, 0, 0, 0, 4, 0, 0, 66, 200, 0, 0, 0, 1, 0, 2, 3, 0, 0, 0, 0, 7, 6, 0, 240, 0, 0,
+            0, 7, 7, 10, 96, 55, 55, 55, 55};
         /* Analysis or forecast at a horizontal level or in a
          * horizontal layer at a point in time. */
         g2int ipdsnum = 0;
@@ -58,13 +46,32 @@ main()
         g2int ngrdpts = 4;
         g2int ibmap = 0;
         g2int bmap[4] = {1, 1, 1, 1};
+        int i;
+        int ret;
+
+        /* Create the message, filling in sections 0 and 1. */
+        if ((ret = g2_create(cgrib, listsec0, listsec1)) != SEC0_LEN + SEC1_LEN)
+            return G2C_ERROR;
+
+        /* Add section 3. */
+        if ((ret = g2_addgrid(cgrib, igds, igdstmpl, NULL, 0)) != 109)
+            return G2C_ERROR;
+
+        /* Add sections 4, 5, and 6. */
         if ((ret = g2_addfield(cgrib, ipdsnum, ipdstmpl, coordlist, numcoord,
                                idrsnum, idrstmpl, fld, ngrdpts, ibmap, bmap)) != MOST_MSG_LEN)
             return G2C_ERROR;
 
+        /* Add section 7. */
         if ((ret = g2_gribend(cgrib)) != FULL_MSG_LEN)
             return G2C_ERROR;
             
+        for (i = 0; i < FULL_MSG_LEN; i++)
+        {
+            /* printf("%d, ", cgrib[i]); */
+            if (cgrib[i] != expected_cgrib[i])
+                return G2C_ERROR;
+        }
     }
     printf("ok!\n");
     printf("SUCCESS!\n");
