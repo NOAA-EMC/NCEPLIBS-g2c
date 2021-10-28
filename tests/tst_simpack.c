75ce39d7ce46c108b4b798f1c74dcc849fd87b28
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-28 11:58:50 -0600

    more simpack testing

--- tests/tst_simpack.c
+++ tests/tst_simpack.c
@@ -69,6 +69,31 @@ main()
 	}
     }
     printf("ok!\n");
+    printf("Testing simpack() call with nbits = 0...");
+    {
+	g2int ndpts = DATA_LEN; 	
+	g2float fld[DATA_LEN] = {1.0, 2.0, 3.0, 0.0};
+	g2float fld_in[DATA_LEN];
+	unsigned char cpack[PACKED_LEN];
+	g2int lcpack;
+        g2int idrstmpl[5] = {0, 1, 1, 0, 0};
+	int i;
+
+	/* Pack the data. */
+	simpack(fld, ndpts, idrstmpl, cpack, &lcpack);
+
+	/* Unpack the data. */
+	if (simunpack(cpack, idrstmpl, ndpts, fld_in))
+	    return G2C_ERROR;
+
+	for (i = 0; i < DATA_LEN; i++)
+	{
+	    /* printf("%g %g\n", fld[i], fld_in[i]); */
+	    if (fld[i] != fld_in[i])
+		return G2C_ERROR;
+	}
+    }
+    printf("ok!\n");
     printf("SUCCESS!\n");
     return 0;
 }
