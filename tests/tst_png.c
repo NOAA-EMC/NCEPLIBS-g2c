9d22c58833d74a9b81e2e5db92bbdc4fb1c8a957
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-29 04:58:59 -0600

    adding test for png functions

---
+++ tests/tst_png.c
@@ -0,0 +1,23 @@
+/* This is a test for the NCEPLIBS-g2c project. This test is for
+ * the PNG compression functions.
+ *
+ * Ed Hartnett 10/29/21
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "grib2.h"
+
+#define G2C_ERROR 2
+
+int
+main()
+{
+    printf("Testing PNG functions.\n");
+    printf("Testing enc_png() call...");
+    {
+    }
+    printf("ok!\n");
+    printf("SUCCESS!\n");
+    return 0;
+}
