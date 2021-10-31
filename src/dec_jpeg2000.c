cd37c0a2ce39d1a8bc8f17af48cd67967e32418a
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-31 08:38:47 -0600

    whitespace cleanup

--- src/dec_jpeg2000.c
+++ src/dec_jpeg2000.c
@@ -11,6 +11,7 @@
  */
 void dummy(void) {}
 #else   /* USE_JPEG2000 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -40,92 +41,80 @@ void dummy(void) {}
  *
  * @author Stephen Gilbert @date 2002-12-02
  */
-int dec_jpeg2000(char *injpc,g2int bufsize,g2int *outfld)
+int dec_jpeg2000(char *injpc, g2int bufsize, g2int *outfld)
 {
-    g2int i,j,k;
+    g2int i, j, k;
     jas_image_t *image=0;
     jas_stream_t *jpcstream;
     jas_image_cmpt_t *pcmpt;
     char *opts=0;
     jas_matrix_t *data;
 
-//    jas_init();
+    /* Create jas_stream_t containing input JPEG200 codestream in
+     * memory. */
+    jpcstream = jas_stream_memopen(injpc, bufsize);
 
-//     Create jas_stream_t containing input JPEG200 codestream in memory.
-//
-
-    jpcstream=jas_stream_memopen(injpc,bufsize);
-
-//
-//     Decode JPEG200 codestream into jas_image_t structure.
-//
-    image=jpc_decode(jpcstream,opts);
+    /* Decode JPEG200 codestream into jas_image_t structure. */
+    image=jpc_decode(jpcstream, opts);
     if ( image == 0 ) {
         printf(" jpc_decode return\n");
         return -3;
     }
 
     pcmpt=image->cmpts_[0];
-/*
-  printf(" SAGOUT DECODE:\n");
-  printf(" tlx %d \n",image->tlx_);
-  printf(" tly %d \n",image->tly_);
-  printf(" brx %d \n",image->brx_);
-  printf(" bry %d \n",image->bry_);
-  printf(" numcmpts %d \n",image->numcmpts_);
-  printf(" maxcmpts %d \n",image->maxcmpts_);
-  #ifdef JAS_1_500_4
-  printf(" colormodel %d \n",image->colormodel_);
-  #endif
-  #ifdef JAS_1_700_2
-  printf(" colorspace %d \n",image->clrspc_);
-  #endif
-  printf(" inmem %d \n",image->inmem_);
-  printf(" COMPONENT:\n");
-  printf(" tlx %d \n",pcmpt->tlx_);
-  printf(" tly %d \n",pcmpt->tly_);
-  printf(" hstep %d \n",pcmpt->hstep_);
-  printf(" vstep %d \n",pcmpt->vstep_);
-  printf(" width %d \n",pcmpt->width_);
-  printf(" height %d \n",pcmpt->height_);
-  printf(" prec %d \n",pcmpt->prec_);
-  printf(" sgnd %d \n",pcmpt->sgnd_);
-  printf(" cps %d \n",pcmpt->cps_);
-  #ifdef JAS_1_700_2
-  printf(" type %d \n",pcmpt->type_);
-  #endif
-*/
+    /*
+      printf(" SAGOUT DECODE:\n");
+      printf(" tlx %d \n", image->tlx_);
+      printf(" tly %d \n", image->tly_);
+      printf(" brx %d \n", image->brx_);
+      printf(" bry %d \n", image->bry_);
+      printf(" numcmpts %d \n", image->numcmpts_);
+      printf(" maxcmpts %d \n", image->maxcmpts_);
+      #ifdef JAS_1_500_4
+      printf(" colormodel %d \n", image->colormodel_);
+      #endif
+      #ifdef JAS_1_700_2
+      printf(" colorspace %d \n", image->clrspc_);
+      #endif
+      printf(" inmem %d \n", image->inmem_);
+      printf(" COMPONENT:\n");
+      printf(" tlx %d \n", pcmpt->tlx_);
+      printf(" tly %d \n", pcmpt->tly_);
+      printf(" hstep %d \n", pcmpt->hstep_);
+      printf(" vstep %d \n", pcmpt->vstep_);
+      printf(" width %d \n", pcmpt->width_);
+      printf(" height %d \n", pcmpt->height_);
+      printf(" prec %d \n", pcmpt->prec_);
+      printf(" sgnd %d \n", pcmpt->sgnd_);
+      printf(" cps %d \n", pcmpt->cps_);
+      #ifdef JAS_1_700_2
+      printf(" type %d \n", pcmpt->type_);
+      #endif
+    */
 
-//   Expecting jpeg2000 image to be grayscale only.
-//   No color components.
-//
+    /* Expecting jpeg2000 image to be grayscale only. No color components. */
     if (image->numcmpts_ != 1 ) {
         printf("dec_jpeg2000: Found color image.  Grayscale expected.\n");
         return (-5);
     }
 
-//
-//    Create a data matrix of grayscale image values decoded from
-//    the jpeg2000 codestream.
-//
+    /* Create a data matrix of grayscale image values decoded from the
+     * jpeg2000 codestream. */
     data=jas_matrix_create(jas_image_height(image), jas_image_width(image));
-    jas_image_readcmpt(image,0,0,0,jas_image_width(image),
-                       jas_image_height(image),data);
-//
-//    Copy data matrix to output integer array.
-//
-    k=0;
-    for (i=0;i<pcmpt->height_;i++)
-        for (j=0;j<pcmpt->width_;j++)
-            outfld[k++]=data->rows_[i][j];
-//
-//     Clean up JasPer work structures.
-//
+    jas_image_readcmpt(image, 0, 0, 0, jas_image_width(image),
+                       jas_image_height(image), data);
+
+    /* Copy data matrix to output integer array. */
+    k = 0;
+    for (i = 0; i < pcmpt->height_; i++)
+        for (j = 0; j < pcmpt->width_; j++)
+            outfld[k++] = data->rows_[i][j];
+
+    /* Clean up JasPer work structures. */
     jas_matrix_destroy(data);
     jas_stream_close(jpcstream);
     jas_image_destroy(image);
 
     return 0;
-
 }
 #endif   /* USE_JPEG2000 */
