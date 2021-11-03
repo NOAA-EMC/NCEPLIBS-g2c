dd73eadb4c37eb902e204ceae11b3eff1a26a6cd
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-11-03 07:52:14 -0600

    more testing

--- tests/tst_com.c
+++ tests/tst_com.c
@@ -418,6 +418,61 @@ main()
         }
     }
     printf("ok!\n");
+    printf("Testing cmplxpack()/comunpack() calls (expect and ignore error messages)...");
+    {
+        g2int len = PACKED_LEN, ndpts = DATA_LEN;
+        g2float fld[DATA_LEN] = {2.0, 2.0, 2.0, 2.0};
+        g2float fld_in[DATA_LEN];
+        unsigned char cpack[PACKED_LEN];
+        g2int lcpack = PACKED_LEN;
+        g2int old_int_val;
+        /* See
+         * https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp5-2.shtml
+         * for the template meaning. */
+        g2int idrstmpl[16] = {
+            0, /* reference value (R) (IEEE 32-bit floating-point value) */
+            1, /* Binary scale factor (E) */
+            1, /* Decimal scale factor (D) */
+            8, /* Number of bits used for each packed value for simple packing, or for each group reference value for complex packing or spatial differencing */
+            0, /* Type of original field values (see Code Table 5.1) */
+            0, /* Group splitting method used (see Code Table 5.4) */
+            1, /* Missing value management used (see Code Table 5.5) */
+            2, /* Primary missing value substitute */
+            3, /* Secondary missing value substitute */
+            1, /* NG ― number of groups of data values into which field is split */
+            16, /* Reference for group widths (see Note 12) */
+            8, /* Number of bits used for the group widths (after the reference value in octet 36 has been removed) */
+            4, /* Reference for group lengths (see Note 13) */
+            0, /* Length increment for the group lengths (see Note 14) */
+            0, /* True length of last group */
+            8 /* Number of bits used for the scaled group lengths (after subtraction of the reference value given in octets 38-41 and division by the length increment given in octet 42) */
+            };
+        g2int idrsnum = 2;
+        int i;
+
+        /* Try with wrong missing value management flag. */
+        old_int_val = idrstmpl[6];
+        idrstmpl[6] = 3;
+        cmplxpack(fld, ndpts, idrsnum, idrstmpl, cpack, &lcpack);
+        if (lcpack != -1)
+            return G2C_ERROR;
+        idrstmpl[6] = old_int_val;
+
+        /* Pack the data. */
+        cmplxpack(fld, ndpts, idrsnum, idrstmpl, cpack, &lcpack);
+
+        /* Unpack the data. */
+        if (comunpack(cpack, len, idrsnum, idrstmpl, ndpts, fld_in))
+            return G2C_ERROR;
+
+        for (i = 0; i < DATA_LEN; i++)
+        {
+            /* printf("%g %g\n", fld[i], fld_in[i]); */
+            if (fld[i] != fld_in[i])
+        	return G2C_ERROR;
+        }
+    }
+    printf("ok!\n");
     printf("SUCCESS!\n");
     return 0;
 }
