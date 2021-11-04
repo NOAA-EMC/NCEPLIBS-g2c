e879cb87f1afdf4842193d10ef8bb9ddccf70564
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-11-04 04:41:08 -0600

    specpack test development

--- tests/tst_spec.c
+++ tests/tst_spec.c
@@ -13,68 +13,54 @@
 #define G2C_ERROR 2
 
 /* Prototypes we are testing. */
+void specpack(g2float *fld, g2int ndpts, g2int JJ, g2int KK, g2int MM, 
+              g2int *idrstmpl, unsigned char *cpack, g2int *lcpack);
+g2int specunpack(unsigned char *cpack, g2int *idrstmpl, g2int ndpts, g2int JJ,
+                 g2int KK, g2int MM, g2float *fld);
 
 int
 main()
 {
     printf("Testing spec functions.\n");
-    /* printf("Testing specpack()/specunpack() calls..."); */
-    /* { */
-    /*     g2int height = 2, width = 2; */
-    /*     g2int len = PACKED_LEN, ndpts = DATA_LEN; */
-    /*     g2float fld[DATA_LEN] = {1.0, 2.0, 3.0, 0.0}; */
-    /*     g2float fld_in[DATA_LEN]; */
-    /*     unsigned char cpack[PACKED_LEN]; */
-    /*     g2int lcpack = PACKED_LEN; */
-    /*     g2int idrstmpl[10] = {0, 1, 1, 16, 0, 1, 2, 3, 4, 5, 6}; */
-    /*     g2int JJ = 1, KK = 2, MM = 3; */
-    /*     int i; */
+    printf("Testing specpack()/specunpack() calls...");
+    {
+        g2int ndpts = DATA_LEN * 2;
+        g2float fld[DATA_LEN * 2] = {1.0, 2.0, 3.0, 0.0, 1.0, 2.0, 3.0, 0.0};
+        g2float fld_in[DATA_LEN * 2];
+        unsigned char cpack[PACKED_LEN];
+        g2int lcpack = PACKED_LEN;
+        /* See
+         * https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp5-51.shtml. */
+        g2int idrstmpl[10] = {
+            0, /* Reference value (R) (IEEE 32-bit floating-point value) */
+            1, /* Binary scale factor (E) */
+            1, /* Decimal scale factor (D) */
+            16, /* Number of bits used for each packed value (field width) */
+            1, /* P― Laplacian scaling factor (expressed in 10-6 units) */
+            1, /* Js ― pentagonal resolution parameter of the unpacked subset (see Note1) */
+            1, /* Ks ― pentagonal resolution parameter of the unpacked subset (see Note1) */
+            1, /* Ms ― pentagonal resolution parameter of the unpacked subset (see Note1) */
+            1, /* Ts ― total number of values in the unpacked subset (see Note1) */
+            1 /* Precision of the unpacked subset (see Code Table 5.7) */
+        };
+        g2int JJ = 1, KK = 2, MM = 3;
+        int i;
 
-    /*     /\* Pack the data. *\/ */
-    /*     specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack); */
+        /* Pack the data. */
+        specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack);
 
-    /*     /\* /\\* Unpack the data. *\\/ *\/ */
-    /*     /\* if (jpcunpack(cpack, len, idrstmpl, ndpts, fld_in)) *\/ */
-    /*     /\*     return G2C_ERROR; *\/ */
+        /* Unpack the data. */
+        if (specunpack(cpack, idrstmpl, ndpts, JJ, KK, MM, fld_in))
+            return G2C_ERROR;
 
-    /*     /\* for (i = 0; i < DATA_LEN; i++) *\/ */
-    /*     /\* { *\/ */
-    /*     /\*     /\\* printf("%g %g\n", fld[i], fld_in[i]); *\\/ *\/ */
-    /*     /\*     if (fld[i] != fld_in[i]) *\/ */
-    /*     /\* 	return G2C_ERROR; *\/ */
-    /*     /\* } *\/ */
-    /* } */
-    /* printf("ok!\n"); */
-    /* printf("Testing enc_jpeg2000() call..."); */
-    /* { */
-    /*     unsigned char data[4] = {1, 2, 3, 4}; */
-    /*     g2int width = 2, height = 2, nbits = 4; */
-    /*     g2int ltype = 0, ratio = 0, retry = 0, jpclen = 200; */
-    /*     char outjpc[200]; */
-    /*     g2int outfld[4]; */
-    /*     /\* int i; *\/ */
-    /*     int ret; */
-
-    /*     /\* Encode some data. *\/ */
-    /*     /\* 168 on Linux, but 133 on windows? *\/ */
-    /*     if ((ret = enc_jpeg2000(data, width, height, nbits, ltype, ratio, retry, outjpc, jpclen)) < 133) */
-    /*     { */
-    /*         printf("%d\n", ret); */
-    /*         return G2C_ERROR; */
-    /*     } */
-
-    /*     /\* Now decode it. *\/ */
-    /*     if ((ret = dec_jpeg2000(outjpc, jpclen, outfld))) */
-    /*     { */
-    /*         printf("%d\n", ret); */
-    /*         return G2C_ERROR; */
-    /*     } */
-
-    /*     /\* for (i = 0; i < 4; i++) *\/ */
-    /*     /\*     if (cout[i] != data[i]) *\/ */
-    /*     /\*         return G2C_ERROR; *\/ */
-    /* } */
-    /* printf("ok!\n"); */
+        for (i = 0; i < DATA_LEN * 2; i++)
+        {
+            printf("%g %g\n", fld[i], fld_in[i]);
+            if (fld[i] != fld_in[i])
+        	return G2C_ERROR;
+        }
+    }
+    printf("ok!\n");
     printf("SUCCESS!\n");
     return 0;
 }
