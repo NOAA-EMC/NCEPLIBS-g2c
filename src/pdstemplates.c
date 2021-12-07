e16a1f9106e3e8d36890aff1e26bab7c732e534c
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-12-07 11:11:01 -0700

    fixed incorrect if-brace

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -381,137 +381,138 @@ extpdstemplate(g2int number, g2int *list)
                 }
             }
         }
-        else if (number == 51)
+    }
+    else if (number == 51)
+    {
+        new->extlen = list[15]*11;
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        for (i = 0; i < list[15]; i++)
         {
-            new->extlen = list[15]*11;
-            new->ext = malloc(sizeof(g2int) * new->extlen);
-            for (i = 0; i < list[15]; i++)
-            {
-                l = i * 6;
-                new->ext[l] = 1;
-                new->ext[l+1] = 1;
-                new->ext[l+2] = -1;
-                new->ext[l+3] = -4;
-                new->ext[l+4] = -1;
-                new->ext[l+5] = -4;
-            }
+            l = i * 6;
+            new->ext[l] = 1;
+            new->ext[l+1] = 1;
+            new->ext[l+2] = -1;
+            new->ext[l+3] = -4;
+            new->ext[l+4] = -1;
+            new->ext[l+5] = -4;
         }
-        else if (number == 33)
+    }
+    else if (number == 33)
+    {
+        new->extlen = list[9];
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        for (i = 0; i < new->extlen; i++)
         {
-            new->extlen = list[9];
-            new->ext = malloc(sizeof(g2int) * new->extlen);
-            for (i = 0; i < new->extlen; i++)
-            {
-                new->ext[i] = 1;
-            }
+            new->ext[i] = 1;
         }
-        else if (number == 34)
+    }
+    else if (number == 34)
+    {
+        new->extlen = ((list[24] - 1) * 6)+list[9];
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        if (list[24] > 1)
         {
-            new->extlen = ((list[24] - 1) * 6)+list[9];
-            new->ext = malloc(sizeof(g2int) * new->extlen);
-            if (list[24] > 1)
+            for (j = 2; j <= list[24]; j++)
             {
-                for (j = 2; j <= list[24]; j++)
+                l = (j - 2) * 6;
+                for (k = 0; k < 6; k++)
                 {
-                    l = (j - 2) * 6;
-                    for (k = 0; k < 6; k++)
-                    {
-                        new->ext[l + k] = new->map[26 + k];
-                    }
+                    new->ext[l + k] = new->map[26 + k];
                 }
             }
-            l = (list[24] - 1) * 6;
-            if (l < 0)
-                l = 0;
-            for (i = 0; i < list[9]; i++)
-            {
-                new->ext[l + i] = 1;
-            }
         }
-        else if (number == 53)
+        l = (list[24] - 1) * 6;
+        if (l < 0)
+            l = 0;
+        for (i = 0; i < list[9]; i++)
         {
-            new->extlen = list[3];
-            new->ext = malloc(sizeof(g2int) * new->extlen);
-            for (i = 0; i < new->extlen; i++)
-            {
-                new->ext[i] = 1;
-            }
+            new->ext[l + i] = 1;
         }
-        else if (number == 54)
+    }
+    else if (number == 53)
+    {
+        new->extlen = list[3];
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        for (i = 0; i < new->extlen; i++)
         {
-            new->extlen = list[3];
-            new->ext = malloc(sizeof(g2int) * new->extlen);
-            for (i = 0;i<new->extlen;i++)
-            {
-                new->ext[i] = 1;
-            }
+            new->ext[i] = 1;
         }
-        else if (number == 91)
+    }
+    else if (number == 54)
+    {
+        new->extlen = list[3];
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        for (i = 0;i<new->extlen;i++)
         {
-            new->extlen = ((list[28] - 1) * 6)+list[15];
-            new->ext = malloc(sizeof(g2int) * new->extlen);
-            if (list[28] > 1)
+            new->ext[i] = 1;
+        }
+    }
+    else if (number == 91)
+    {
+        new->extlen = ((list[28] - 1) * 6)+list[15];
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        if (list[28] > 1)
+        {
+            for (j = 2; j <= list[28]; j++)
             {
-                for (j = 2; j <= list[28]; j++)
+                l = (j - 2) * 6;
+                for (k = 0; k < 6; k++)
                 {
-                    l = (j - 2) * 6;
-                    for (k = 0; k < 6; k++)
-                    {
-                        new->ext[l + k] = new->map[30 + k];
-                    }
+                    new->ext[l + k] = new->map[30 + k];
                 }
             }
-            l = (list[29] - 1) * 6;
-            if (l < 0)
-                l = 0;
-            for (i = 0; i < list[15]; i++)
-            {
-                new->ext[l + i] = 1;
-            }
         }
-        /* PDT 4.57  (10/07/2015) */
-        else if (number == 57)
+        l = (list[29] - 1) * 6;
+        if (l < 0)
+            l = 0;
+        for (i = 0; i < list[15]; i++)
         {
-            new->extlen = list[6] * 15;
+            new->ext[l + i] = 1;
+        }
+    }
+    /* PDT 4.57  (10/07/2015) */
+    else if (number == 57)
+    {
+        new->extlen = list[6] * 15;
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        for (i = 0; i < list[6]; i++)
+        {
+            l = i*15;
+            new->ext[l] = 1;
+            new->ext[l+1] = -4;
+            new->ext[l+2] = 1;
+            new->ext[l+3] = 1;
+            new->ext[l+4] = 1;
+            new->ext[l+5] = 2;
+            new->ext[l+6] = 1;
+            new->ext[l+7] = 1;
+            new->ext[l+8] = -4;
+            new->ext[l+9] = 1;
+            new->ext[l+10] = -1;
+            new->ext[l+11] = -4;
+            new->ext[l+12] = 1;
+            new->ext[l+13] = -1;
+            new->ext[l+14] = -4;
+        }
+    }
+    /* PDT 4.61  (10/07/2015) */
+    else if (number == 61)
+    {
+        if (list[30] > 1)
+        {
+            new->extlen = (list[30] - 1) * 6;
             new->ext = malloc(sizeof(g2int) * new->extlen);
-            for (i = 0; i < list[6]; i++)
+            for (j = 2; j <= list[30]; j++)
             {
-                l = i*15;
-                new->ext[l] = 1;
-                new->ext[l+1] = -4;
-                new->ext[l+2] = 1;
-                new->ext[l+3] = 1;
-                new->ext[l+4] = 1;
-                new->ext[l+5] = 2;
-                new->ext[l+6] = 1;
-                new->ext[l+7] = 1;
-                new->ext[l+8] = -4;
-                new->ext[l+9] = 1;
-                new->ext[l+10] = -1;
-                new->ext[l+11] = -4;
-                new->ext[l+12] = 1;
-                new->ext[l+13] = -1;
-                new->ext[l+14] = -4;
-            }
-        }
-        /* PDT 4.61  (10/07/2015) */
-        else if (number == 61)
-        {
-            if (list[30] > 1)
-            {
-                new->extlen = (list[30] - 1) * 6;
-                new->ext = malloc(sizeof(g2int) * new->extlen);
-                for (j = 2; j <= list[30]; j++)
+                l = (j - 2) * 6;
+                for (k = 0; k < 6; k++)
                 {
-                    l = (j - 2) * 6;
-                    for (k = 0; k < 6; k++)
-                    {
-                        new->ext[l + k] = new->map[32 + k];
-                    }
+                    new->ext[l + k] = new->map[32 + k];
                 }
             }
         }
     }
+}
 
-    return new;
+return new;
 }
