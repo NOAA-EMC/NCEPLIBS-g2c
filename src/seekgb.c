f89ba9baaca0fcdc98ab27f7d1f7609f6edfeafb
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2021-12-09 05:47:19 -0700

    adding test for seekgb() (#242)
    * adding test for seekgb
    
    * first test
    
    * more testing
    
    * improved comments
    
    * improved comments
    
    * improved comments

--- src/seekgb.c
+++ src/seekgb.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include "grib2_int.h"
 
+/** 8 bits per byte. */
+#define BITS_PER_BYTE 8
+
 /**
  * This subprogram searches a file for the next GRIB Message. The
  * search is done starting at byte offset iseek of the file referenced
@@ -14,14 +17,17 @@
  * respectively. The search is terminated when an EOF or I/O error is
  * encountered.
  *
- * PROGRAM HISTORY LOG:
- * - 2002-10-28  GILBERT   Modified from Iredell's skgb subroutine
- * - 2009-01-16  VUONG     Changed  lskip to 4 instead of sizof(g2int)
+ * ### Program History Log
+ * Date | Programmer | Comments
+ * -----|------------|---------
+ * 2002-10-28 | GILBERT | Modified from Iredell's skgb subroutine
+ * 2009-01-16 | VUONG | Changed  lskip to 4 instead of sizof(g2int)
  *
  * @param lugb FILE pointer for the file to search. File must be
  * opened before this routine is called.
  * @param iseek number of bytes in the file to skip before search.
- * @param mseek number of bytes to search at a time.
+ * @param mseek number of bytes to search at a time (must be at least
+ * 16).
  * @param lskip number of bytes to skip from the beggining of the file
  * to where the GRIB message starts.
  * @param lgrib number of bytes in message (set to 0, if no message
@@ -46,26 +52,33 @@ seekgb(FILE *lugb, g2int iseek, g2int mseek, g2int *lskip, g2int *lgrib)
     {
         /* Read partial section. */
         fseek(lugb, ipos, SEEK_SET);
-        nread=fread(cbuf, sizeof(unsigned char), mseek, lugb);
-        lim=nread-8;
+        nread = fread(cbuf, sizeof(unsigned char), mseek, lugb);
+        lim = nread - 8;
 
         /* Look for 'grib...' in partial section. */
         for (k = 0; k < lim; k++)
         {
-            gbit(cbuf, &start, (k + 0) * 8, 4 * 8);
-            gbit(cbuf, &vers, (k + 7) * 8, 1 * 8);
-            
+            /* Look at the first 4 bytes - should be 'GRIB'. */
+            gbit(cbuf, &start, k * BITS_PER_BYTE, 4 * BITS_PER_BYTE);
+
+            /* Look at the 8th byte, it has the GRIB version. */
+            gbit(cbuf, &vers, (k + 7) * BITS_PER_BYTE, 1 * BITS_PER_BYTE);
+
+            /* If the message starts with 'GRIB', and is version 1 or
+             * 2, then this is a GRIB message. */
             if (start == 1196575042 && (vers == 1 || vers == 2))
             {
-                /*  Look for '7777' at end of grib message. */
+                /* Find the length of the message. */
                 if (vers == 1)
-                    gbit(cbuf, &lengrib, (k + 4) * 8, 3 * 8);
+                    gbit(cbuf, &lengrib, (k + 4) * BITS_PER_BYTE, 3 * BITS_PER_BYTE);
                 if (vers == 2)
-                    gbit(cbuf, &lengrib, (k + 12) * 8, 4 * 8);
+                    gbit(cbuf, &lengrib, (k + 12) * BITS_PER_BYTE, 4 * BITS_PER_BYTE);
+
+                /* Read the last 4 bytesof the message. */
                 fseek(lugb, ipos + k + lengrib - 4, SEEK_SET);
-                
-                /* Hard code to 4 instead of sizeof(g2int). */
                 k4 = fread(&end, 4, 1, lugb);
+                
+                /* Look for '7777' at end of grib message. */
                 if (k4 == 1 && end == 926365495)
                 {
                     /* GRIB message found. */
---
+++ tests/tst_seekgb.c
@@ -0,0 +1,61 @@
+/* This is a test for the NCEPLIBS-g2c project. This test is for
+ * seekgb.c.
+ *
+ * Ed Hartnett 12/9/21
+ */
+
+#include <stdio.h>
+#include "grib2_int.h"
+
+#define G2C_ERROR 2
+
+#define GRIB2_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2"
+#define GRIB2_INDEX_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2.idx"
+
+int
+main()
+{
+    printf("Testing seekgb.\n");
+    printf("Testing simple seekgb() call...");
+    {
+        FILE *f;
+        g2int lskip, lgrib;
+
+        /* Open test data file. */
+        if (!(f = fopen(GRIB2_FILE, "r")))
+            return G2C_ERROR;
+
+        /* Run seekgb() on data file. */
+        seekgb(f, 0, 16, &lskip, &lgrib);
+
+        /* Check results. */
+        if (lgrib != 15254 || lskip)
+            return G2C_ERROR;
+
+        /* Close test data file. */
+        fclose(f);
+    }
+    printf("ok!\n");
+    printf("Testing seekgb() call on non-data file...");
+    {
+        FILE *f;
+        g2int lskip, lgrib;
+
+        /* Open test data file. */
+        if (!(f = fopen(GRIB2_INDEX_FILE, "r")))
+            return G2C_ERROR;
+
+        /* Run seekgb() on data file. */
+        seekgb(f, 0, 16, &lskip, &lgrib);
+
+        /* Check results. */
+        if (lgrib != 0)
+            return G2C_ERROR;
+
+        /* Close test data file. */
+        fclose(f);
+    }
+    printf("ok!\n");
+    printf("SUCCESS!\n");
+    return 0;
+}
