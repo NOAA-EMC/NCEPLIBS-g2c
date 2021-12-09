4b4a390fde289c0790350f056c29e5c0f56559b0
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-12-09 04:58:02 -0700

    first test

--- tests/tst_seekgb.c
+++ tests/tst_seekgb.c
@@ -9,12 +9,31 @@
 
 #define G2C_ERROR 2
 
+#define GRIB2_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2"
+#define GRIB2_INDEX_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2.idx"
+
 int
 main()
 {
     printf("Testing sbit.\n");
     printf("Testing simple seekgb() call...");
     {
+        FILE *f;
+        g2int lskip, lgrib;
+
+        /* Open test data file. */
+        if (!(f = fopen(GRIB2_FILE, "r")))
+            return G2C_ERROR;
+
+        /* Run seekgb() on data file. */
+        seekgb(f, 0, 32, &lskip, &lgrib);
+
+        /* Check results. */
+        if (lgrib != 15254 || lskip)
+            return G2C_ERROR;
+
+        /* Close test data file. */
+        fclose(f);
     }
     printf("ok!\n");
     printf("SUCCESS!\n");
