6f4d4fbc6cd9979f46849243890bd8fb2eff0da3
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2022-04-18 07:46:50 -0600

    correct usage of jasper by using jas_image_encode()/jas_image_decode() instead of jpc_encode()/jpc_decode(), also initialize and finalize jasper (#250)
    * using jas_image_encode() instead of jpc_encode()
    
    * cleanup and documentation
    
    * converted jpc_decode() to jas_image_decode()
    
    * cleanup
    
    * cleaned up documentation
    
    * cleaned up documentation
    
    * cleaned up documentation

--- src/enc_jpeg2000.c
+++ src/enc_jpeg2000.c
@@ -13,15 +13,14 @@
 /**
  * This Function encodes a grayscale image into a JPEG2000 code stream
  * specified in the JPEG2000 Part-1 standard (i.e., ISO/IEC 15444-1)
- * using JasPer Software version 1.500.4 (or 1.700.2) written by the
- * University of British Columbia, Image Power Inc, and others.
- * JasPer is available at http://www.ece.uvic.ca/~mdadams/jasper/.
+ * using [JasPer Software](https://github.com/jasper-software/jasper).
  *
  * ### Program History Log
  * Date | Programmer | Comments
  * -----|------------|---------
  * 2002-12-02 | Gilbert | Initial
  * 2004-12-16 | Gilbert | Added retry argument allowing increased guard bits.
+ * 2022-04-15 | Hartnett | Converted to use jas_ instead of jpc_ functions.
  *
  * @param cin Packed matrix of Grayscale image values to encode.
  * @param width width of image.
@@ -41,11 +40,13 @@
  *
  * @return
  * - > 0 = Length in bytes of encoded JPEG2000 code stream
- * - -3 = Error encode jpeg2000 code stream.
+ * - ::G2_JASPER_INIT Error initializing jasper library.
+ * - ::G2_JASPER_ENCODE Error encode jpeg2000 code stream.
  *
  * @note Requires JasPer Software version 1.500.4 or 1.700.2 or later.
  *
  * @author Stephen Gilbert @date 2002-12-02
+ * @author Ed Hartnett
  */
 int
 enc_jpeg2000(unsigned char *cin, g2int width, g2int height, g2int nbits,
@@ -57,6 +58,7 @@ enc_jpeg2000(unsigned char *cin, g2int width, g2int height, g2int nbits,
     jas_stream_t *jpcstream, *istream;
     jas_image_cmpt_t cmpt, *pcmpt;
     char opts[MAXOPTSSIZE];
+    int fmt;
 
     /* Set lossy compression options, if requested. */
     if (ltype != 1)
@@ -92,6 +94,10 @@ enc_jpeg2000(unsigned char *cin, g2int width, g2int height, g2int nbits,
     pcmpt = &cmpt;
     image.cmpts_ = &pcmpt;
 
+    /* Initialize Jasper. */
+    if (jas_init())
+        return G2_JASPER_INIT;
+
     /* Open a JasPer stream containing the input grayscale values. */
     istream = jas_stream_memopen((char *)cin, height * width * cmpt.cps_);
     cmpt.stream_ = istream;
@@ -100,18 +106,24 @@ enc_jpeg2000(unsigned char *cin, g2int width, g2int height, g2int nbits,
      * code stream. */
     jpcstream = jas_stream_memopen(outjpc, (int)jpclen);
 
+    /* Get jasper ID of JPEG encoder. */
+    fmt = jas_image_strtofmt(G2C_JASPER_JPEG_FORMAT_NAME);
+
     /* Encode image. */
-    if ((ier = jpc_encode(&image, jpcstream, opts)))
-    {
-        printf(" jpc_encode return = %d \n",ier);
-        return -3;
-    }
+    if ((ier = jas_image_encode(&image, jpcstream, fmt, opts)))
+        return G2_JASPER_ENCODE;
+
+    /* Rememeber the length in bytes of the encoded JPEG code
+     * stream. */
+    rwcnt = jpcstream->rwcnt_;
 
     /* Clean up JasPer work structures. */
-    rwcnt = jpcstream->rwcnt_;
     ier = jas_stream_close(istream);
     ier = jas_stream_close(jpcstream);
 
+    /* Finalize jasper. */
+    jas_cleanup();
+
     /* Return size of jpeg2000 code stream. */
-    return (rwcnt);
+    return rwcnt;
 }
