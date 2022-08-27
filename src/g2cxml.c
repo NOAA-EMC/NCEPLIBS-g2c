030886925c5d7d316a3be78f0ea479563fc30b11
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-27 06:01:40 -0600

    adding libxml2 to build

--- src/g2cxml.c
+++ src/g2cxml.c
@@ -4,3 +4,46 @@
  * @author Ed Hartnett @date 8/25/22
  */
 
+#include <libxml/tree.h>
+#include <libxml/parser.h>
+#include <grib2_int.h>
+
+/**
+ * Init.
+ *
+ * @return
+ * - ::G2C_NOERROR No error.
+ *
+ * @author Ed Hartnett @date 8/26/22
+ */
+int
+init()
+{
+    xmlDocPtr doc;
+    xmlNodePtr cur;
+
+    doc = xmlParseFile("CodeFlag.xml");
+	
+    if (doc == NULL )
+    {
+	fprintf(stderr,"Document not parsed successfully. \n");
+	return G2C_EXML;
+    }
+
+    cur = xmlDocGetRootElement(doc);
+	
+    if (cur == NULL)
+    {
+	fprintf(stderr,"empty document\n");
+	xmlFreeDoc(doc);
+	return G2C_EXML;
+    }
+	
+    if (xmlStrcmp(cur->name, (const xmlChar *) "story")) {
+	fprintf(stderr,"document of the wrong type, root node != story");
+	xmlFreeDoc(doc);
+	return G2C_EXML;
+    }
+
+    return G2C_NOERROR;
+}
