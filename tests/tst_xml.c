c68e27b63f9d6b89cdb9f01abe4210a6c6bc3271
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-27 06:54:29 -0600

    adding XML code test

---
+++ tests/tst_xml.c
@@ -0,0 +1,16 @@
+/* This is a test for the NCEPLIBS-g2c project. This test is for
+ * g2cxml.c.
+ *
+ * Ed Hartnett 8/27/22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "grib2_int.h"
+
+int
+main()
+{
+    return G2C_NOERROR;
+}
+
