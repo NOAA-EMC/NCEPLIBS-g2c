320a563f6909a4d1a9608f17d3a76a70d81e0427
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-30 16:22:44 -0600

    more testing

--- tests/tst_error.c
+++ tests/tst_error.c
@@ -12,15 +12,36 @@
 
 int main()
 {
-    char error_string[MAX_LEN + 1];
-    
     printf("Testing error handling.\n");
 
-    strncpy(error_string, g2c_strerror(0), MAX_LEN);
-    if (strncmp(error_string, "No error", MAX_LEN))
+    if (strncmp(g2c_strerror(0), "No error", MAX_LEN))
 	return G2C_ERROR;
-
-    printf("%s\n", g2c_strerror(0));
+    if (strncmp(g2c_strerror(-50), "GRIB2 header not found", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-51), "GRIB message is already complete.", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-52), "Name too long", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-53), "Invalid input", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-54), "Error reading file", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-55), "Bad ID", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-56), "Too many files open", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-57), "Out of memory", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-58), "Error decoding message", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-59), "No GRIB message found", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-60), "Error parsing XML", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(-61), "Table or entry not found", MAX_LEN))
+	return G2C_ERROR;
+    if (strncmp(g2c_strerror(999), "Unknown Error", MAX_LEN))
+	return G2C_ERROR;
 
     printf("SUCCESS!\n");
     return 0;
