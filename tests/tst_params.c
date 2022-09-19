74a7d4a405a13cfecd2640453d4485ce727a43a0
Edward Hartnett <edward.hartnett@noaa.gov>
2022-09-19 10:30:05 -0600

    adding g2cparams.c

---
+++ tests/tst_params.c
@@ -0,0 +1,25 @@
+/* This is a test for the NCEPLIBS-g2c project. This test is for
+ * g2cparams.c.
+ *
+ * Ed Hartnett 9/19/22
+ */
+#include "grib2_int.h"
+
+int
+main()
+{
+    char abbrev[G2C_MAX_NOAA_ABBREV_LEN + 1];
+    int ret;
+
+    printf("Testing params handling.\n");
+
+    printf("Testing g2c_param_abbrev()...\n");
+    if ((ret = g2c_param_abbrev(10, 3, 209, abbrev)))
+        return ret;
+    if (strcmp(abbrev, "TWLDT"))
+        return G2C_ERROR;
+    
+    printf("SUCCESS!!!\n");
+    return G2C_NOERROR;
+}
+
