d729490a6cde29e00da05b245568c918449c3c77
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-05 11:31:51 -0600

    now comparing degrib2 output to reference file

--- tests/tst_degrib2.c
+++ tests/tst_degrib2.c
@@ -11,6 +11,37 @@
 
 #define FILE_NAME "tst_degrib2.txt"
 #define WAVE_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2"
+#define REF_FILE "ref_gdaswave.degrib2.txt"
+
+/* Return 0 if two files are the same. */
+int
+compare_files(char *fname1, char *fname2)
+{
+   FILE *fp1, *fp2;
+   int ch1, ch2;
+   int ret = G2C_NOERROR;
+ 
+   if (!(fp1 = fopen(fname1, "r")))
+       return G2C_ERROR;
+   if (!(fp2 = fopen(fname2, "r")))
+       return G2C_ERROR;
+ 
+   ch1 = getc(fp1);
+   ch2 = getc(fp2);
+   while ((ch1 != EOF) && (ch2 != EOF) && (ch1 == ch2))
+   {
+       ch1 = getc(fp1);
+       ch2 = getc(fp2);
+   }
+ 
+   if (ch1 != ch2)
+       ret = G2C_ERROR;
+
+   fclose(fp1);
+   fclose(fp2);
+   
+   return ret;
+}
 
 int
 main()
@@ -28,6 +59,8 @@ main()
             return ret;
 	if ((ret = g2c_close(g2cid)))
 	    return ret;
+        if ((ret = compare_files(FILE_NAME, REF_FILE)))
+            return ret;
     }
     printf("ok!\n");
 #endif
