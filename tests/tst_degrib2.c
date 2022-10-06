742fb3c5dca20275aca8be4bb7065d8cfd48ef68
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-06 05:42:19 -0600

    turning test back on

--- tests/tst_degrib2.c
+++ tests/tst_degrib2.c
@@ -28,6 +28,7 @@ compare_files(char *fname1, char *fname2)
  
    ch1 = getc(fp1);
    ch2 = getc(fp2);
+   printf("%d %d\n", ch1, ch2);
    while ((ch1 != EOF) && (ch2 != EOF) && (ch1 == ch2))
    {
        ch1 = getc(fp1);
