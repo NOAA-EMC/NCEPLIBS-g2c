6b8e9c875e87347fb371b307d36b2935d9670c42
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-17 12:56:20 +0200

    improved the way big-endian values are read from file with macros

--- src/grib2_int.h
+++ src/grib2_int.h
@@ -45,6 +45,11 @@
 #define BYTE 8 /**< Number of bits in a byte. */
 #define WORD 32 /**< Number of bits in four bytes. */
 
+#define ONE_BYTE 1 /**< One byte. */
+#define TWO_BYTES 2 /**< Two bytes. */
+#define FOUR_BYTES 4 /**< Four bytes. */
+#define EIGHT_BYTES 8 /**< Eight bytes. */
+
 /** Byte swap 64-bit ints. This converts big-endian 8-byte ints into
  * native endian 8-byte ints. */
 #define bswap64(y) (((uint64_t)ntohl(y)) << 32 | ntohl(y>>32))
@@ -86,11 +91,6 @@
 		    return G2C_EFILE;				\
 		var = bswap64(size_t_be);			\
 	    } while(0)
-
-#define ONE_BYTE 1 /**< One byte. */
-#define TWO_BYTES 2 /**< Two bytes. */
-#define FOUR_BYTES 4 /**< Four bytes. */
-#define EIGHT_BYTES 8 /**< Eight bytes. */
 
 /** This is the information about each message. */
 typedef struct g2c_message_info
