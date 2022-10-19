f35a458a2477e6e2036399fc23271571b07154ae
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-19 14:39:37 +0200

    porting index code from branch

---
+++ tests/tst_index.c
@@ -0,0 +1,43 @@
+/* This is a test for the NCEPLIBS-g2c project. 
+ *
+ * This test is for the g2c_write_index()/g2c_read_index() functions,
+ * which write and read and index file that contians the byte offsets
+ * to various parts of a GRIB2 file.
+ *
+ * Ed Hartnett 10/13/22
+ */
+#include "grib2_int.h"
+
+#define FILE_NAME "tst_index.txt"
+#define WAVE_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2"
+#define REF_FILE "ref_gdaswave.t00z.wcoast.0p16.f000.grb2index"
+
+int
+main()
+{
+    printf("Testing g2c index functions.\n");
+#ifdef JPEG
+    printf("Testing g2c_read_index() on file %s...", WAVE_FILE);
+    {
+	int g2cid;
+	int ret;
+
+	/* Will not work. */
+	if (g2c_read_index(NULL, &g2cid) != G2C_EINVAL)
+	    return G2C_ERROR;
+	if (g2c_read_index("bad", &g2cid) != G2C_EFILE)
+	    return G2C_ERROR;
+
+	/* This will work. */
+	g2c_set_log_level(10);
+	if ((ret = g2c_read_index(REF_FILE, &g2cid)))
+	    return ret;
+	/* if ((ret = g2c_close(g2cid))) */
+	/*     return ret; */
+    }
+    printf("ok!\n");
+#endif
+    printf("SUCCESS!\n");
+    return 0;
+}
+
