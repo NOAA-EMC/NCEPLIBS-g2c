058f1c169d8d27ea307bca3c48caad819802ee07
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-19 18:50:00 +0200

    working on CI

--- src/g2cindex.c
+++ src/g2cindex.c
@@ -31,7 +31,7 @@ int
 g2c_read_section1_metadata_2(FILE *f, size_t skip, G2C_MESSAGE_INFO_T *msg)
 {
     int int_be;
-    /* short short_be; */
+    short short_be;
     char sec_num;
 
     /* Skip to section 1. */
@@ -41,9 +41,9 @@ g2c_read_section1_metadata_2(FILE *f, size_t skip, G2C_MESSAGE_INFO_T *msg)
     /* Read the section. */
     READ_BE_INT4(f, msg->sec1_len);
     READ_BE_INT1(f, sec_num);
-    /* if (sec_num != 1) */
-    /*     return G2C_ENOSECTION; */
-    /* READ_BE_INT2(f, msg->center); */
+    if (sec_num != 1)
+        return G2C_ENOSECTION;
+    READ_BE_INT2(f, msg->center);
     /* READ_BE_INT2(f, msg->subcenter); */
     /* READ_BE_INT1(f, msg->master_version); */
     /* READ_BE_INT1(f, msg->local_version); */
