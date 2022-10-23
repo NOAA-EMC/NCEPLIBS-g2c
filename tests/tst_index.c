282bd5eb3862df9b42518bb1607a979a356f9bc6
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-23 13:28:07 -0600

    uncommented test code

--- tests/tst_index.c
+++ tests/tst_index.c
@@ -20,25 +20,25 @@ main()
     printf("Testing g2c_read_index() on file %s...", WAVE_FILE);
     {
 	int g2cid;
-        /* int num_msg; */
+        int num_msg;
 	int ret;
 
-	/* /\* Will not work. *\/ */
-	/* if (g2c_read_index(NULL, NULL, 0, &g2cid) != G2C_EINVAL) */
-	/*     return G2C_ERROR; */
-	/* if (g2c_read_index("bad", NULL, 0, &g2cid) != G2C_EINVAL) */
-	/*     return G2C_ERROR; */
-	/* if (g2c_read_index("bad", "bad", 0, &g2cid) != G2C_EFILE) */
-	/*     return G2C_ERROR; */
+	/* Will not work. */
+	if (g2c_read_index(NULL, NULL, 0, &g2cid) != G2C_EINVAL)
+	    return G2C_ERROR;
+	if (g2c_read_index("bad", NULL, 0, &g2cid) != G2C_EINVAL)
+	    return G2C_ERROR;
+	if (g2c_read_index("bad", "bad", 0, &g2cid) != G2C_EFILE)
+	    return G2C_ERROR;
 
 	/* This will work. */
 	g2c_set_log_level(10);
 	if ((ret = g2c_read_index(WAVE_FILE, REF_FILE, 0, &g2cid)))
 	    return ret;
-        /* /\* if ((ret = g2c_inq(g2cid, &num_msg))) *\/ */
-        /* /\*     return ret; *\/ */
-        /* /\* if (num_msg != 19) *\/ */
-        /* /\*     return G2C_ERROR; *\/ */
+        if ((ret = g2c_inq(g2cid, &num_msg)))
+            return ret;
+        if (num_msg != 19)
+            return G2C_ERROR;
 	if ((ret = g2c_close(g2cid)))
 	    return ret;
     }
