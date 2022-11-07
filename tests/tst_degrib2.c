03249188ce23b088976e711447c3c2858f70506a
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-07 07:56:20 -0700

    added new test file

--- tests/tst_degrib2.c
+++ tests/tst_degrib2.c
@@ -17,11 +17,16 @@
 #define REF_FTP_INDEX_FILE "ref_WW3_Regional_US_West_Coast_20220718_0000.grb2index"
 #define FTP_DEGRIB2_FILE "WW3_Regional_US_West_Coast_20220718_0000.degrib2"
 #define REF_FTP_DEGRIB2_FILE "ref_WW3_Regional_US_West_Coast_20220718_0000.degrib2"
+#define FTP_NUM_MSG 688
+
+#define FTP_FILE_2 "gdas.t12z.pgrb2.1p00.anl.grib2"
+#define FTP_DEGRIB2_FILE_2 "gdas.t12z.pgrb2.1p00.anl.degrib2"
+#define REF_FTP_DEGRIB2_FILE_2 "ref_gdas.t12z.pgrb2.1p00.anl.degrib2"
+#define FTP_NUM_MSG_2 2
 
 #define MAX_LINE_LEN 256
 #define MAX_VALUE_LEN 25
 #define NUM_MATCHING 5
-#define FTP_NUM_MSG 688
 
 /* Return 0 if two lines of the DEGRIB2 output are euqal. 
  * 
@@ -196,60 +201,144 @@ main()
 #endif
 #ifdef FTP_TEST_FILES
 #define NUM_TESTS 2
-    printf("Testing degrib2 on file %s downloaded via FTP...\n", FTP_FILE);
+    /* printf("Testing degrib2 on file %s downloaded via FTP...\n", FTP_FILE); */
+    /* { */
+    /*     int g2cid; */
+    /*     int num_msg; */
+    /*     unsigned char sig_ref_time, month, day, hour, minute, second; */
+    /*     short year; */
+    /*     short center, subcenter; */
+    /*     unsigned char master_version, local_version; */
+    /*     int num_fields, num_local; */
+    /*     unsigned char discipline; */
+    /*     unsigned char expected_discipline[FTP_NUM_MSG] = */
+    /*         {10, 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10, */
+    /*          10, 0, 0, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 0, 0, 10, 10, 10, */
+    /*          10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, */
+    /*          0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 0, 0, 0, 0, 10, 10, */
+    /*          10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 10, 10, */
+    /*          10, 10, 0, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, */
+    /*          10, 10, 10, 10, 0, 0, 0, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10, */
+    /*          10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10, 10, */
+    /*          0, 10, 10, 10, 0, 10, 0, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, */
+    /*          10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 0, 10, 10, 0, 0, 10, 0, */
+    /*          10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10, 10, 10, 0, 0, 10, */
+    /*          10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, */
+    /*          0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, */
+    /*          10, 10, 10, 10, 0, 0, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 0, */
+    /*          10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 0, 0, 0, */
+    /*          10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 0, 0, 10, 10, 10, 10, */
+    /*          10, 10, 10, 0, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 0, 10, */
+    /*          10, 10, 10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, */
+    /*          10, 10, 0, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 0, 10, 0, */
+    /*          10, 10, 0, 10, 10, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, */
+    /*          10, 0, 10, 10, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, */
+    /*          10, 0, 10, 10, 10, 0, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, */
+    /*          10, 10, 10, 0, 10, 0, 10, 10, 10, 0, 10, 10, 0, 10, 10, 0, 10, 0, */
+    /*          10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 0, 10, 10, */
+    /*          0, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, */
+    /*          10, 10, 10, 0, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, */
+    /*          10, 10, 0, 10, 10, 0, 10, 10, 10, 10, 10, 10, 0, 0, 0, 10, 10, 10, */
+    /*          10, 10, 10, 0, 10, 0, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, 0, 10, */
+    /*          10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 10, */
+    /*          10, 10, 0, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 10, 10, 10, */
+    /*          10, 10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, */
+    /*          10, 10, 0, 0, 0, 10, 10, 0, 10, 0, 0, 10, 0, 10, 10, 10, 10, 0, 10, */
+    /*          10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 0, 10, 10, 10, 0, */
+    /*          10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 10, 10, 10, 10, 10, 10, 10, */
+    /*          0, 10, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, 10, 10, 0, 0, 0, 10, */
+    /*          10, 10, 10, 0, 10, 10, 10, 0, 10, 0, 0, 0, 10, 0, 10, 10, 10, 10, */
+    /*          10, 10, 10, 10, 0, 10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 0, 10, 0, */
+    /*          10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, 10, 10, */
+    /*          10, 0, 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10}; */
+    /*     int t, m; */
+    /*     int ret; */
+
+    /*     for (t = 0; t < NUM_TESTS; t++) */
+    /*     { */
+    /*         /\* g2c_set_log_level(11); *\/ */
+    /*         /\* Open the data file with and without the index file. *\/ */
+    /*         if (t) */
+    /*         { */
+    /*             printf("\ttesting degrib2 on file %s downloaded via FTP using index...", FTP_FILE); */
+    /*             if ((ret = g2c_read_index(FTP_FILE, REF_FTP_INDEX_FILE, 0, &g2cid))) */
+    /*                 return ret; */
+    /*         } */
+    /*         else */
+    /*         { */
+    /*             printf("\ttesting degrib2 on file %s downloaded via FTP without using index...", FTP_FILE); */
+    /*             if ((ret = g2c_open(FTP_FILE, 0, &g2cid))) */
+    /*                 return ret; */
+    /*         } */
+            
+    /*         /\* Check some stuff. *\/ */
+    /*         if ((ret = g2c_inq(g2cid, &num_msg))) */
+    /*             return ret; */
+    /*         if (num_msg != FTP_NUM_MSG) */
+    /*             return G2C_ERROR; */
+
+    /*         /\* Check some stuff in all messages. *\/ */
+    /*         for (m = 0; m < num_msg; m++) */
+    /*         { */
+    /*             if ((ret = g2c_inq_msg(g2cid, m, &discipline, &num_fields, &num_local, */
+    /*                                    &center, &subcenter, &master_version, &local_version))) */
+    /*                 return ret; */
+    /*             if (center != 7 || subcenter != 0 || master_version != 2 || local_version != 1) */
+    /*                 return G2C_ERROR; */
+    /*             if (num_fields != 1 || num_local) */
+    /*                 return G2C_ERROR; */
+    /*             if (discipline != expected_discipline[m]) */
+    /*                 return G2C_ERROR; */
+    /*         } */
+            
+    /*         /\* Check some stuff about the last message. *\/ */
+    /*         if ((ret = g2c_inq_msg(g2cid, 687, &discipline, &num_fields, &num_local, */
+    /*                                &center, &subcenter, &master_version, &local_version))) */
+    /*             return ret; */
+    /*         if (center != 7 || subcenter != 0 || master_version != 2 || local_version != 1) */
+    /*             return G2C_ERROR; */
+    /*         if (num_local || num_fields != 1 || discipline != 10) */
+    /*             return G2C_ERROR; */
+            
+    /*         /\* Inquire about the date/time. *\/ */
+    /*         if ((ret = g2c_inq_msg_time(g2cid, 687, &sig_ref_time, &year, &month, &day, &hour, */
+    /*                                     &minute, &second))) */
+    /*             return ret; */
+            
+    /*         /\* Check date/time. *\/ */
+    /*         if (sig_ref_time != 1 || year != 2022 || month != 7 || day != 18 || */
+    /*             hour != 0 || minute != 0 || second != 0) */
+    /*             return G2C_ERROR; */
+            
+    /*         /\* Output a degrib2 file. *\/ */
+    /*         if ((ret = g2c_degrib2(g2cid, FTP_DEGRIB2_FILE))) */
+    /*             return ret; */
+
+    /*         /\* Close the file. *\/ */
+    /*         if ((ret = g2c_close(g2cid))) */
+    /*             return ret; */
+
+    /*         /\* Compare the degrib2 output to our reference file. *\/ */
+    /*         if ((ret = compare_files2(FTP_DEGRIB2_FILE, REF_FTP_DEGRIB2_FILE))) */
+    /*             return ret; */
+            
+    /*         printf("\tok!\n"); */
+    /*     } */
+    /* } */
+    /* printf("ok!\n"); */
+    printf("Testing degrib2 on file %s downloaded via FTP...\n", FTP_FILE_2);
     {
 	int g2cid;
         int num_msg;
-        unsigned char sig_ref_time, month, day, hour, minute, second;
-        short year;
         short center, subcenter;
         unsigned char master_version, local_version;
         int num_fields, num_local;
         unsigned char discipline;
-        unsigned char expected_discipline[FTP_NUM_MSG] =
-            {10, 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10,
-             10, 0, 0, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 0, 0, 10, 10, 10,
-             10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0,
-             0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 0, 0, 0, 0, 10, 10,
-             10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 10, 10,
-             10, 10, 0, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0,
-             10, 10, 10, 10, 0, 0, 0, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10,
-             10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10, 10,
-             0, 10, 10, 10, 0, 10, 0, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10,
-             10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 0, 10, 10, 0, 0, 10, 0,
-             10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 0, 10, 10, 10, 10, 0, 0, 10,
-             10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10,
-             0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10,
-             10, 10, 10, 10, 0, 0, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 0,
-             10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 0, 0, 0,
-             10, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 0, 0, 10, 10, 10, 10,
-             10, 10, 10, 0, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 0, 10,
-             10, 10, 10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10,
-             10, 10, 0, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 0, 10, 0,
-             10, 10, 0, 10, 10, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10,
-             10, 0, 10, 10, 0, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10,
-             10, 0, 10, 10, 10, 0, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10,
-             10, 10, 10, 0, 10, 0, 10, 10, 10, 0, 10, 10, 0, 10, 10, 0, 10, 0,
-             10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 0, 10, 10,
-             0, 0, 10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10, 0,
-             10, 10, 10, 0, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 10, 10,
-             10, 10, 0, 10, 10, 0, 10, 10, 10, 10, 10, 10, 0, 0, 0, 10, 10, 10,
-             10, 10, 10, 0, 10, 0, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, 0, 10,
-             10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 0, 10,
-             10, 10, 0, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 10, 10, 10, 10,
-             10, 10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-             10, 10, 0, 0, 0, 10, 10, 0, 10, 0, 0, 10, 0, 10, 10, 10, 10, 0, 10,
-             10, 10, 10, 10, 10, 10, 10, 0, 10, 10, 10, 10, 10, 0, 10, 10, 10, 0,
-             10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 0, 10, 10, 10, 10, 10, 10, 10,
-             0, 10, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, 10, 10, 0, 0, 0, 10,
-             10, 10, 10, 0, 10, 10, 10, 0, 10, 0, 0, 0, 10, 0, 10, 10, 10, 10,
-             10, 10, 10, 10, 0, 10, 0, 10, 0, 10, 10, 10, 10, 10, 10, 0, 10, 0,
-             10, 10, 10, 10, 10, 10, 10, 10, 0, 0, 10, 10, 10, 10, 0, 10, 10,
-             10, 0, 10, 10, 10, 0, 10, 10, 10, 0, 10, 10, 10, 10};
         int t, m;
 	int ret;
 
-        for (t = 0; t < NUM_TESTS; t++)
+        /* for (t = 0; t < NUM_TESTS; t++) */
+        for (t = 0; t < 1; t++)
         {
             /* g2c_set_log_level(11); */
             /* Open the data file with and without the index file. */
@@ -261,15 +350,15 @@ main()
             }
             else
             {
-                printf("\ttesting degrib2 on file %s downloaded via FTP without using index...", FTP_FILE);
-                if ((ret = g2c_open(FTP_FILE, 0, &g2cid)))
+                printf("\ttesting degrib2 on file %s downloaded via FTP without using index...", FTP_FILE_2);
+                if ((ret = g2c_open(FTP_FILE_2, 0, &g2cid)))
                     return ret;
             }
             
             /* Check some stuff. */
             if ((ret = g2c_inq(g2cid, &num_msg)))
                 return ret;
-            if (num_msg != FTP_NUM_MSG)
+            if (num_msg != FTP_NUM_MSG_2)
                 return G2C_ERROR;
 
             /* Check some stuff in all messages. */
@@ -278,35 +367,44 @@ main()
                 if ((ret = g2c_inq_msg(g2cid, m, &discipline, &num_fields, &num_local,
                                        &center, &subcenter, &master_version, &local_version)))
                     return ret;
+                /* printf("discipline %d num_fields %d num_local %d center %d subcenter %d master_version %d local_version %d\n", */
+                /*        discipline, num_fields, num_local, center, subcenter, master_version, local_version); */
                 if (center != 7 || subcenter != 0 || master_version != 2 || local_version != 1)
                     return G2C_ERROR;
                 if (num_fields != 1 || num_local)
                     return G2C_ERROR;
-                if (discipline != expected_discipline[m])
+                if (discipline != 0)
                     return G2C_ERROR;
             }
             
             /* Check some stuff about the last message. */
-            if ((ret = g2c_inq_msg(g2cid, 687, &discipline, &num_fields, &num_local,
+            if ((ret = g2c_inq_msg(g2cid, 1, &discipline, &num_fields, &num_local,
                                    &center, &subcenter, &master_version, &local_version)))
                 return ret;
             if (center != 7 || subcenter != 0 || master_version != 2 || local_version != 1)
                 return G2C_ERROR;
-            if (num_local || num_fields != 1 || discipline != 10)
+            if (num_local || num_fields != 1 || discipline != 0)
                 return G2C_ERROR;
             
             /* Inquire about the date/time. */
-            if ((ret = g2c_inq_msg_time(g2cid, 687, &sig_ref_time, &year, &month, &day, &hour,
-                                        &minute, &second)))
-                return ret;
+            {
+                unsigned char sig_ref_time, month, day, hour, minute, second;
+                short year;
+                
+                if ((ret = g2c_inq_msg_time(g2cid, 1, &sig_ref_time, &year, &month, &day, &hour,
+                                            &minute, &second)))
+                    return ret;
             
-            /* Check date/time. */
-            if (sig_ref_time != 1 || year != 2022 || month != 7 || day != 18 ||
-                hour != 0 || minute != 0 || second != 0)
-                return G2C_ERROR;
+                /* Check date/time. */
+                /* printf("sig_ref_time %d year %d month %d day %d hour %d minute %d second %d\n", */
+                /*        sig_ref_time, year, month, day, hour, minute, second); */
+                if (sig_ref_time != 0 || year != 2022 || month != 11 || day != 6 ||
+                    hour != 12 || minute != 0 || second != 0)
+                    return G2C_ERROR;
+            }
             
             /* Output a degrib2 file. */
-            if ((ret = g2c_degrib2(g2cid, FTP_DEGRIB2_FILE)))
+            if ((ret = g2c_degrib2(g2cid, FTP_DEGRIB2_FILE_2)))
                 return ret;
 
             /* Close the file. */
@@ -314,7 +412,7 @@ main()
                 return ret;
 
             /* Compare the degrib2 output to our reference file. */
-            if ((ret = compare_files2(FTP_DEGRIB2_FILE, REF_FTP_DEGRIB2_FILE)))
+            if ((ret = compare_files2(FTP_DEGRIB2_FILE_2, REF_FTP_DEGRIB2_FILE_2)))
                 return ret;
             
             printf("\tok!\n");
