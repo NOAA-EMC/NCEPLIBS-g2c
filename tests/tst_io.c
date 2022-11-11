44499d1b37412a3f685e612d3e4f777dc2929f1a
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-11 10:22:09 -0700

    adding tests

--- tests/tst_io.c
+++ tests/tst_io.c
@@ -13,7 +13,7 @@ int
 main()
 {
     printf("Testing g2c I/O functions.\n");
-    printf("Testing simple calls with 4-byte ints...\n");
+    printf("Testing simple calls with 4-byte ints...");
     {
         FILE *f;
         int val = 42;
@@ -37,10 +37,6 @@ main()
         /* Close file. */
         fclose(f);
 
-        /* Reposition at start of file. */
-        /* if (fseek(f, 0, SEEK_SET)) */
-        /*     return G2C_EFILE; */
-
         /* Open the test file. */
         if (!(f = fopen(TEST_FILE, "rb")))
             return G2C_EFILE;
@@ -57,6 +53,69 @@ main()
         if ((ret = g2c_file_be_uint4(f, G2C_FILE_READ, &uval_in)))
             return ret;
         if (uval_in != uval)
+            return G2C_ERROR;
+
+        /* Reposition at start of file. */
+        if (fseek(f, 0, SEEK_SET))
+            return G2C_EFILE;
+
+        /* Read as template values. */
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_READ, 0, &val_in)))
+            return ret;
+        if (val_in != val)
+            return G2C_ERROR;
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_READ, 1, &val_in)))
+            return ret;
+        if (val_in != neg_val)
+            return G2C_ERROR;
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_READ, 0, &val_in)))
+            return ret;
+        if (val_in != val)
+            return G2C_ERROR;
+
+        /* Close file. */
+        fclose(f);
+    }
+    printf("ok!\n");
+    printf("Testing template calls with 4-byte ints...");
+    {
+        FILE *f;
+        int val = 42;
+        int neg_val = -42;
+        int val_in;
+        int ret;
+
+        /* Open the test file. */
+        if (!(f = fopen(TEST_FILE, "wb")))
+            return G2C_EFILE;
+
+        /* Write 4 bytes, thrice. */
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_WRITE, 0, &val)))
+            return ret;
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_WRITE, 1, &neg_val)))
+            return ret;
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_WRITE, 0, &val)))
+            return ret;
+
+        /* Close file. */
+        fclose(f);
+
+        /* Open the test file. */
+        if (!(f = fopen(TEST_FILE, "rb")))
+            return G2C_EFILE;
+
+        /* Read as template values. */
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_READ, 0, &val_in)))
+            return ret;
+        if (val_in != val)
+            return G2C_ERROR;
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_READ, 1, &val_in)))
+            return ret;
+        if (val_in != neg_val)
+            return G2C_ERROR;
+        if ((ret = g2c_file_template_int4(f, G2C_FILE_READ, 0, &val_in)))
+            return ret;
+        if (val_in != val)
             return G2C_ERROR;
 
         /* Close file. */
