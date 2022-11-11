c335a009284d6e5f74d0b3b1a2a7fc9ce2ebd2f1
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-11 10:06:05 -0700

    adding tests

--- tests/tst_io.c
+++ tests/tst_io.c
@@ -13,19 +13,50 @@ int
 main()
 {
     printf("Testing g2c I/O functions.\n");
-    printf("Testing simple calls...\n");
+    printf("Testing simple calls with 4-byte ints...\n");
     {
         FILE *f;
         int val = 42;
+        int neg_val = -42;
+        int val_in;
         int ret;
 
         /* Open the test file. */
         if (!(f = fopen(TEST_FILE, "wb")))
             return G2C_EFILE;
 
-        /* Write 4 bytes. */
+        /* Write 4 bytes, thrice. */
         if ((ret = g2c_file_be_int4(f, G2C_FILE_WRITE, &val)))
             return ret;
+        if ((ret = g2c_file_be_int4(f, G2C_FILE_WRITE, &neg_val)))
+            return ret;
+        if ((ret = g2c_file_be_int4(f, G2C_FILE_WRITE, &val)))
+            return ret;
+
+        /* Close file. */
+        fclose(f);
+
+        /* Reposition at start of file. */
+        /* if (fseek(f, 0, SEEK_SET)) */
+        /*     return G2C_EFILE; */
+
+        /* Open the test file. */
+        if (!(f = fopen(TEST_FILE, "rb")))
+            return G2C_EFILE;
+
+        /* Read three values. */
+        if ((ret = g2c_file_be_int4(f, G2C_FILE_READ, &val_in)))
+            return ret;
+        if (val_in != val)
+            return G2C_ERROR;
+        if ((ret = g2c_file_be_int4(f, G2C_FILE_READ, &val_in)))
+            return ret;
+        if (val_in != neg_val)
+            return G2C_ERROR;
+        if ((ret = g2c_file_be_int4(f, G2C_FILE_READ, &val_in)))
+            return ret;
+        if (val_in != val)
+            return G2C_ERROR;
 
         /* Close file. */
         fclose(f);
