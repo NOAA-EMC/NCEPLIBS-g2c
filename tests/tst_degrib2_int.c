5475d97ad57a8e5e5feed615e96fb4e932a0d883
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-16 07:11:36 -0700

    adding test

--- tests/tst_degrib2_int.c
+++ tests/tst_degrib2_int.c
@@ -23,12 +23,33 @@
 #define NUM_MATCHING 5
 #define FTP_NUM_MSG 688
 
+#define NUM_PROD_TEST 1
 int
 main()
 {
     printf("Testing g2c degrib2 internal functions.\n");
     printf("Testing g2c_get_level_desc()...");
     {
+        int prod_template_num[NUM_PROD_TEST] = {0};
+        int prod_template_value[NUM_PROD_TEST][G2C_MAX_PDS_TEMPLATE_MAPLEN] = {
+            {2, 1, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0}
+        };
+        char expected_level_desc[NUM_PROD_TEST][G2C_MAX_GRIB_LEVEL_DESC_LEN + 1] = {
+            "Surface"};
+        char level_desc[G2C_MAX_GRIB_LEVEL_DESC_LEN + 1];
+        int t;
+        int ret;
+
+        for (t = 0; t < NUM_PROD_TEST; t++)
+        {
+            if ((ret = g2c_get_level_desc(prod_template_num[t], prod_template_value[t], level_desc)))
+                return ret;
+            if (strcmp(level_desc, expected_level_desc[t]))
+            {
+                printf("level_desc %s expected %s\n", level_desc, expected_level_desc[t]);
+                return G2C_ERROR;
+            }
+        }
     }
     printf("ok!\n");
     printf("SUCCESS!\n");
