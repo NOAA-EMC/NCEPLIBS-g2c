d7f4373ce2414714abacd1e6503bb8326961c524
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-16 08:17:49 -0700

    more testing

--- tests/tst_degrib2_int.c
+++ tests/tst_degrib2_int.c
@@ -7,23 +7,8 @@
  */
 #include "grib2_int.h"
 
-#define FILE_NAME "tst_degrib2.txt"
-#define WAVE_FILE "gdaswave.t00z.wcoast.0p16.f000.grib2"
-#define REF_FILE "ref_gdaswave.degrib2.txt"
-#define DEGRIB2_FILE "gdaswave.t00z.wcoast.0p16.f000.degrib2"
-#define REF_INDEX_FILE "ref_gdaswave.t00z.wcoast.0p16.f000.grb2index"
-#define TEST_INDEX_FILE "tst_gdaswave.t00z.wcoast.0p16.f000.grb2index"
-#define FTP_FILE "WW3_Regional_US_West_Coast_20220718_0000.grib2"
-#define REF_FTP_INDEX_FILE "ref_WW3_Regional_US_West_Coast_20220718_0000.grb2index"
-#define FTP_DEGRIB2_FILE "WW3_Regional_US_West_Coast_20220718_0000.degrib2"
-#define REF_FTP_DEGRIB2_FILE "ref_WW3_Regional_US_West_Coast_20220718_0000.degrib2"
-
-#define MAX_LINE_LEN 256
-#define MAX_VALUE_LEN 25
-#define NUM_MATCHING 5
-#define FTP_NUM_MSG 688
-
 #define NUM_PROD_TEST 2
+
 int
 main()
 {
