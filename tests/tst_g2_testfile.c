d9cdafe57c33d0e540199e7934fee6feb661b52f
Edward Hartnett <edward.hartnett@noaa.gov>
2022-12-02 11:08:03 -0700

    adding test based on g2 testfile

---
+++ tests/tst_g2_testfile.c
@@ -0,0 +1,19 @@
+/* This is a test for the NCEPLIBS-g2c project. This test reads a test
+ * file produced by the tests of the NCEPLIBS-g2 library.
+ *
+ * Ed Hartnett 12/2/22
+ */
+
+#include "grib2_int.h"
+
+int
+main()
+{
+    printf("Testing with file created in NCEPLIBS-g2.\n");
+    printf("testing opening of the file...");
+    {
+    }
+    printf("ok!\n");
+    printf("SUCCESS!\n");
+    return 0;
+}
