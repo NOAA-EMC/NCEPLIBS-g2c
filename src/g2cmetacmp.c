d600954b43ceb782192e4e62c852e88ccc012fe5
Edward Hartnett <edward.hartnett@noaa.gov>
2022-12-28 10:10:40 -0700

    adding g2c_metadata_cmp() function and test

--- src/g2cmetacmp.c
+++ src/g2cmetacmp.c
@@ -50,6 +50,8 @@ g2c_metadata_cmp(int g2cid1, int g2cid2)
     msg2 = g2c_file[g2cid2].msg;
     for (m = 0; m < g2c_file[g2cid1].num_messages; m++)
     {
+        int fld;
+        
         if (msg1->discipline != msg2->discipline)
             return G2C_ERROR;
         if (msg1->center != msg2->center || msg1->subcenter != msg2->subcenter ||
@@ -59,171 +61,94 @@ g2c_metadata_cmp(int g2cid1, int g2cid2)
             msg1->minute != msg2->minute || msg1->second != msg2->second || msg1->status != msg2->status ||
             msg1->type != msg2->type)
             return G2C_ERROR;
-        
-        /*     int fld; */
+        if (msg1->num_local != msg2->num_local || msg1->num_fields != msg2->num_fields)
+            return G2C_ERROR;
 
-        /*     fprintf(f, "  SECTION 1:  %d %d %d %d %d %d %d %d %d %d %d %d %d\n", msg->center, msg->subcenter, */
-        /*             msg->master_version, msg->local_version, msg->sig_ref_time, msg->year, msg->month, msg->day, */
-        /*             msg->hour, msg->minute, msg->second, msg->status, msg->type); */
-        /*     fprintf(f, "  Contains  %d  Local Sections  and  %d  data fields.\n", msg->num_local, msg->num_fields); */
-        /*     LOG((5, "  SECTION 1:  %d %d %d %d %d %d %d %d %d %d %d %d %d\n", msg->center, msg->subcenter, */
-        /*          msg->master_version, msg->local_version, msg->sig_ref_time, msg->year, msg->month, msg->day, */
-        /*          msg->hour, msg->minute, msg->second, msg->status, msg->type)); */
+            /* For each field, print info. */
+        for (fld = 0; fld < msg1->num_fields; fld++)
+        {
+            G2C_SECTION_INFO_T *sec_1, *sec_2;
+            G2C_SECTION_INFO_T *sec3_1, *sec3_2;
+            G2C_SECTION_INFO_T *sec5_1, *sec5_2;
+            G2C_SECTION3_INFO_T *sec3_info_1, *sec3_info_2;
+            G2C_SECTION4_INFO_T *sec4_info_1, *sec4_info_2;
+            G2C_SECTION5_INFO_T *sec5_info_1, *sec5_info_2;
+            int t;
 
-        /*     /\* For each field, print info. *\/ */
-        /*     for (fld = 0; fld < msg->num_fields; fld++) */
-        /*     { */
-        /*         G2C_SECTION_INFO_T *sec, *sec2, *sec3, *sec5, *sec6; */
-        /*         G2C_SECTION3_INFO_T *sec3_info; */
-        /*         G2C_SECTION4_INFO_T *sec4_info; */
-        /*         G2C_SECTION5_INFO_T *sec5_info; */
-        /*         G2C_SECTION6_INFO_T *sec6_info; */
-        /*         char abbrev[G2C_MAX_NOAA_ABBREV_LEN + 1]; */
-        /*         char level_desc[G2C_MAX_TYPE_OF_FIXED_SURFACE_LEN + 1]; */
-        /*         char date_time[100 + 1]; */
-        /*         float *data; */
-        /*         float total, max, min; */
-        /*         int t; */
+            /* Find this field (a.k.a. product, a.k.a. section 4). */
+            for (sec_1 = msg1->sec; sec_1; sec_1 = sec_1->next)
+            {
+                sec4_info_1 = (G2C_SECTION4_INFO_T *)sec_1->sec_info;
+                if (sec_1->sec_num == 4 && sec4_info_1->field_num == fld)
+                    break;
+            }
+            if (!sec_1)
+                return G2C_ENOSECTION;
+            for (sec_2 = msg2->sec; sec_2; sec_2 = sec_2->next)
+            {
+                sec4_info_2 = (G2C_SECTION4_INFO_T *)sec_2->sec_info;
+                if (sec_2->sec_num == 4 && sec4_info_2->field_num == fld)
+                    break;
+            }
+            if (!sec_2)
+                return G2C_ENOSECTION;
+            if (sec4_info_1->num_coord != sec4_info_2->num_coord ||
+                sec4_info_1->prod_def != sec4_info_2->prod_def)
+                return G2C_ERROR;
+            if (sec_1->template_len != sec_2->template_len)
+                return G2C_ERROR;
+            for (t = 0; t < sec_1->template_len; t++)
+                if (sec_1->template[t] != sec_2->template[t])
+                    return G2C_ERROR;                    
 
-        /*         fprintf(f, "\n"); */
-        /*         fprintf(f, "  FIELD  %d\n", fld + 1); */
-        /*         fprintf(f, "  SECTION 0:  %d 2\n", msg->discipline); */
-        /*         fprintf(f, "  SECTION 1:  %d %d %d %d %d %d %d %d %d %d %d %d %d\n", msg->center, msg->subcenter, */
-        /*                 msg->master_version, msg->local_version, msg->sig_ref_time, msg->year, msg->month, msg->day, */
-        /*                 msg->hour, msg->minute, msg->second, msg->status, msg->type); */
+            /* Find the sec3 that applies to this field. */
+            for (sec3_1 = sec_1; sec3_1; sec3_1 = sec3_1->prev)
+                if (sec3_1->sec_num == 3)
+                    break;
+            if (!sec3_1)
+                return G2C_ENOSECTION;
+            sec3_info_1 = (G2C_SECTION3_INFO_T *)sec3_1->sec_info;
+            for (sec3_2 = sec_2; sec3_2; sec3_2 = sec3_2->prev)
+                if (sec3_2->sec_num == 3)
+                    break;
+            if (!sec3_2)
+                return G2C_ENOSECTION;
+            sec3_info_2 = (G2C_SECTION3_INFO_T *)sec3_2->sec_info;
+            if (sec3_info_1->source_grid_def != sec3_info_2->source_grid_def ||
+                sec3_info_1->num_data_points != sec3_info_2->num_data_points ||
+                sec3_info_1->num_opt != sec3_info_2->num_opt ||
+                sec3_info_1->interp_list != sec3_info_2->interp_list ||
+                sec3_info_1->grid_def != sec3_info_2->grid_def)
+                return G2C_ERROR;
+            if (sec3_1->template_len != sec3_2->template_len)
+                return G2C_ERROR;
+            for (t = 0; t < sec3_1->template_len; t++)
+                if (sec3_1->template[t] != sec3_2->template[t])
+                    return G2C_ERROR;                    
+            
+            /* Find the sec5 that applies to this field. */
+            for (sec5_1 = sec_1; sec5_1; sec5_1 = sec5_1->next)
+                if (sec5_1->sec_num == 5)
+                    break;
+            if (!sec5_1)
+                return G2C_ENOSECTION;
+            sec5_info_1 = (G2C_SECTION5_INFO_T *)sec5_1->sec_info;
+            for (sec5_2 = sec_2; sec5_2; sec5_2 = sec5_2->next)
+                if (sec5_2->sec_num == 5)
+                    break;
+            if (!sec5_2)
+                return G2C_ENOSECTION;
+            sec5_info_2 = (G2C_SECTION5_INFO_T *)sec5_2->sec_info;
+            if (sec5_info_1->num_data_points != sec5_info_2->num_data_points ||
+                sec5_info_1->data_def != sec5_info_2->data_def)
+                return G2C_ERROR;
+            if (sec5_1->template_len != sec5_2->template_len)
+                return G2C_ERROR;
+            for (t = 0; t < sec5_1->template_len; t++)
+                if (sec5_1->template[t] != sec5_2->template[t])
+                    return G2C_ERROR;                    
 
-        /*         /\* Find this field (a.k.a. product, a.k.a. section 4). *\/ */
-        /*         for (sec = msg->sec; sec; sec = sec->next) */
-        /*             if (sec->sec_num == 4 && ((G2C_SECTION4_INFO_T *)(sec->sec_info))->field_num == fld) */
-        /*                 break; */
-        /*         if (!sec) */
-        /*             return G2C_ENOSECTION; */
-
-        /*         /\* Is there a local section? *\/ */
-        /*         for (sec2 = sec; sec2; sec2 = sec2->prev) */
-        /*             if (sec2->sec_num == 2) */
-        /*                 break; */
-        /*         if (sec2) */
-        /*             fprintf(f, "  SECTION 2: %d bytes\n", sec2->sec_len - 5); /\* Subtract 4 byte seclen and 1 byte secnum. *\/ */
-
-        /*         /\* Find the sec3 that applies to this field. *\/ */
-        /*         for (sec3 = sec; sec3; sec3 = sec3->prev) */
-        /*             if (sec3->sec_num == 3) */
-        /*                 break; */
-        /*         if (!sec3) */
-        /*             return G2C_ENOSECTION; */
-
-        /*         /\* Print the section 3 (grid) info. *\/ */
-        /*         sec3_info = (G2C_SECTION3_INFO_T *)sec3->sec_info; */
-        /*         fprintf(f, "  SECTION 3:  %d %d %d %d %d\n", sec3_info->source_grid_def, sec3_info->num_data_points, sec3_info->num_opt, */
-        /*                 sec3_info->interp_list, sec3_info->grid_def); */
-        /*         fprintf(f, "  GRID TEMPLATE 3. %d : ", sec3_info->grid_def); */
-        /*         /\* Cast to int to match behavior of  degrib2.F90. *\/ */
-        /*         for (t = 0; t < sec3->template_len; t++) */
-        /*             fprintf(f, " %d", (int)sec3->template[t]); */
-        /*         fprintf(f, "\n"); */
-        /*         if (!sec3_info->optional) */
-        /*             fprintf(f, "  NO Optional List Defining Number of Data Points.\n"); */
-
-        /*         /\* Print the section 4 (product) info. *\/ */
-        /*         sec4_info = (G2C_SECTION4_INFO_T *)sec->sec_info; */
-        /*         fprintf(f, "  PRODUCT TEMPLATE 4. %d: ", sec4_info->prod_def); */
-
-        /*         /\* We can look up the parameter abbreviation with the */
-        /*          * discipline and the first two elements of the product */
-        /*          * template, which will be the category and product */
-        /*          * number. *\/ */
-        /*         if ((ret = g2c_param_abbrev(msg->discipline, sec->template[0], sec->template[1], abbrev))) */
-        /*             return ret; */
-        /*         fprintf(f, "( PARAMETER = %-8s %d %lld %lld ) ", abbrev, sec->msg->discipline, sec->template[0], */
-        /*                 sec->template[1]); */
-        /*         /\* Cast to int to match behavior of  degrib2.F90. *\/ */
-        /*         for (t = 0; t < sec->template_len; t++) */
-        /*             fprintf(f, " %d", (int)sec->template[t]); */
-        /*         fprintf(f, "\n"); */
-
-        /*         /\* Using the product template number, and the template */
-        /*          * values, we can figure out a description for the */
-        /*          * horizontal level description. *\/ */
-        /*         if ((ret = g2c_get_level_desc(sec4_info->prod_def, sec->template, level_desc))) */
-        /*             return ret; */
-
-        /*         /\* The Fortran code for level descriptions puts a space in */
-        /*          * front of level descriptions that don't start with a */
-        /*          * number. It's annoying, but do it here to match the */
-        /*          * behavior of Fortran degrib2. *\/ */
-        /*         /\* if (!isdigit(level_desc[0])) *\/ */
-        /*         /\* { *\/ */
-        /*         /\*     char tmp_level_desc[G2C_MAX_TYPE_OF_FIXED_SURFACE_LEN + 2]; *\/ */
-        /*         /\*     sprintf(tmp_level_desc, " %s", level_desc); *\/ */
-        /*         /\*     strcpy(level_desc, tmp_level_desc); *\/ */
-        /*         /\* } *\/ */
-
-        /*         /\* Put the date/time in a formatted string. *\/ */
-        /*         if ((ret = g2c_get_datetime(sec4_info->prod_def, sec->template, msg->year, msg->month, msg->day, */
-        /*                                     msg->hour, msg->minute, msg->second, date_time))) */
-        /*             return ret; */
-        /*         fprintf(f, "  FIELD: %-8s%s %s\n", abbrev, level_desc, date_time); */
-        /*         if (!sec4_info->num_coord) */
-        /*             fprintf(f, "  NO Optional Vertical Coordinate List.\n"); */
-
-        /*         /\* Find the sec5 that applies to this field. *\/ */
-        /*         for (sec5 = sec; sec5; sec5 = sec5->next) */
-        /*             if (sec5->sec_num == 5) */
-        /*                 break; */
-        /*         if (!sec5) */
-        /*             return G2C_ENOSECTION; */
-
-        /*         /\* Find the sec6 that applies to this field, if any. *\/ */
-        /*         for (sec6 = sec; sec6; sec6 = sec6->next) */
-        /*             if (sec6->sec_num == 6) */
-        /*                 break; */
-
-        /*         /\* Section 5 and 6 info. *\/ */
-        /*         sec5_info = (G2C_SECTION5_INFO_T *)sec5->sec_info; */
-        /*         sec6_info = (G2C_SECTION6_INFO_T *)sec6->sec_info; */
-        /*         if (sec6_info->indicator != 255) */
-        /*             fprintf(f, "  Num. of Data Points =  %d    with BIT-MAP  0\n", sec5_info->num_data_points); */
-        /*         else */
-        /*             fprintf(f, "  Num. of Data Points =  %d     NO BIT-MAP \n", sec5_info->num_data_points); */
-        /*         fprintf(f, "  DRS TEMPLATE 5. %d : ", sec5_info->data_def); */
-        /*         /\* Cast to int to match behavior of  degrib2.F90. *\/ */
-        /*         for (t = 0; t < sec5->template_len; t++) */
-        /*             fprintf(f, " %d", (int)sec5->template[t]); */
-        /*         fprintf(f, "\n"); */
-        /*         fprintf(f, "  Data Values:\n"); */
-        /*         fprintf(f, "  Num. of Data Points =  %d   Num. of Data Undefined = 0\n", sec5_info->num_data_points); */
-
-        /*         /\* Now read the data and find the min, max, and average values. *\/ */
-
-        /*         /\* Allocate storage for the data. *\/ */
-        /*         if (!(data = malloc(sec5_info->num_data_points * sizeof(float)))) */
-        /*             return G2C_ERROR; */
-
-        /*         /\* Get the data from message 0, product 0. *\/ */
-        /*         if ((ret = g2c_get_prod(g2cid, msg->msg_num, fld, NULL, data))) */
-        /*             return ret; */
-
-        /*         /\* Find min/max/avg. *\/ */
-        /*         max = data[0]; */
-        /*         min = data[0]; */
-        /*         total = data[0]; */
-        /*         for (i = 1; i < sec5_info->num_data_points; i++) */
-        /*         { */
-        /*             total += data[i]; */
-        /*             if (data[i] > max) */
-        /*                 max = data[i]; */
-        /*             if (data[i] < min) */
-        /*                 min = data[i]; */
-        /*         } */
-        /*         fprintf(f, "( PARM= %s ) :  MIN=%25.8f AVE=%25.8f MAX=%25.8f\n", */
-        /*                 abbrev, min, total/sec5_info->num_data_points, max); */
-
-        /*         /\* Free the data. *\/ */
-        /*         free(data); */
-
-        /*         total_fields++; */
-        /*     } */
+        }
         msg1 = msg1->next;
         msg2 = msg2->next;
     }
