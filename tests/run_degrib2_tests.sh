afd8f838c8e1faa0a6c1de4e797c95800952d124
Edward Hartnett <edward.hartnett@noaa.gov>
2022-12-29 12:26:27 -0700

    more testing

--- tests/run_degrib2_tests.sh
+++ tests/run_degrib2_tests.sh
@@ -12,5 +12,8 @@ echo "*** Running g2c_degrib2 test"
 # Generate degrib2 output for our test GRIB2 file.
 ../utils/g2c_degrib2 -v data/gdaswave.t00z.wcoast.0p16.f000.grib2 g2c_degrib2_gdaswave.t00z.wcoast.0p16.f000.grib2.degrib2 
 
+# Check against expected output.
+diff -w g2c_degrib2_gdaswave.t00z.wcoast.0p16.f000.grib2.degrib2 data/ref_gdaswave.degrib2.txt
+
 echo "*** SUCCESS!"
 exit 0
