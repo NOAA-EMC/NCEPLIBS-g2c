74e36aade47cd1974e963cc009de112b5938c7e8
Edward Hartnett <edward.hartnett@noaa.gov>
2022-12-29 09:47:22 -0700

    starting to add utils

---
+++ tests/run_degrib2_tests.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# This is a test script for the NCEPLIBS-g2c project.
+#
+# This script tests the g2c_degrib2 utility.
+#
+# Ed Hartnett, 12/29/22
+
+set -e
+echo ""
+echo "*** Running g2c_convert test"
+
+# # Convert test file to GRIB1.
+# ../src/cnvgrib/cnvgrib -g21 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000.grib1
+
+# # Generate an inventory of the GRIB1 file.
+# ../src/wgrib/wgrib test_gdaswave.t00z.wcoast.0p16.f000.grib1 &> test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt
+
+# # Check against expected output.
+# cmp test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt data/ref_gdaswave_grib1_inventory.txt
+
+# # Convert GRIB1 output back to GRIB2.
+# ../src/cnvgrib/cnvgrib -g12 test_gdaswave.t00z.wcoast.0p16.f000.grib1 test_gdaswave.t00z.wcoast.0p16.f000.grib2
+
+# # Create an index of a GRIB2 file.
+# ../src/grb2index/grb2index test_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx
+
+# # Check against expected output. First 120 bytes contain differences,
+# # so ignore them.
+# cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2.idx
+
+# # Convert test file to another GRIB2 file.
+# ../src/cnvgrib/cnvgrib -g22 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2
+
+# # Create an index of the new GRIB2 file.
+# ../src/grb2index/grb2index test_gdaswave.t00z.wcoast.0p16.f000_2.grib2 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx
+
+# # Check against expected output. First 120 bytes contain differences,
+# # so ignore them.
+# cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx
+
+echo "*** SUCCESS!"
+exit 0
