c6784da0200a7595ec2770aec237fcc95847488d
Edward Hartnett <edward.hartnett@noaa.gov>
2023-01-09 09:12:40 +0100

    adding test

--- tests/tst_jpeg.c
+++ tests/tst_jpeg.c
@@ -10,6 +10,7 @@
 
 #define DATA_LEN 4
 #define PACKED_LEN 200
+#define EPSILON .001
 
 int
 main()
@@ -217,6 +218,42 @@ main()
         {
             /* printf("%g %g\n", fld[i], fld_in[i]); */
             if (fld[i] != fld_in[i])
+        	return G2C_ERROR;
+        }
+    }
+    printf("ok!\n");
+    printf("Testing g2c_jpcpackd()/g2c_jpcunpackd() call with drstmpl values as in NCEPLIBS-g2 test_jpcpack...");
+    {
+        size_t height = 2, width = 2;
+        size_t len = PACKED_LEN, ndpts = DATA_LEN;
+        double fld[DATA_LEN] = {1.1, 2.2, 3.3, 4.4};
+        double fld_in[DATA_LEN];
+        unsigned char cpack[PACKED_LEN];
+        size_t lcpack = PACKED_LEN;
+        /* See
+         * https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp5-40.shtml */
+        int idrstmpl[7] = {
+            0, /* Reference value (R) (IEEE 32-bit floating-point value) */
+            1, /* Binary scale factor (E) */
+            1, /* Decimal scale factor (D) */
+            32, /* Number of bits required to hold the resulting scaled and referenced data values. (i.e. The depth of the grayscale image.) (see Note 2) */
+            0, /* Type of original field values (see Code Table 5.1) */
+            0, /* Type of Compression used. (see Code Table 5.40) */
+            1 /* Target compression ratio, M:1 (with respect to the bit-depth specified in octet 20), when octet 22 indicates Lossy Compression. Otherwise, set to missing (see Note 3) */
+        };
+        int i;
+
+        /* Pack the data. */
+        g2c_jpcpackd(fld, width, height, idrstmpl, cpack, &lcpack);
+
+        /* Unpack the data. */
+        if (g2c_jpcunpackd(cpack, len, idrstmpl, ndpts, fld_in))
+            return G2C_ERROR;
+
+        for (i = 0; i < DATA_LEN; i++)
+        {
+            printf("%g %g\n", fld[i], fld_in[i]);
+            if (abs(fld[i] - fld_in[i]) > EPSILON)
         	return G2C_ERROR;
         }
     }
