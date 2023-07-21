d08a57967d3104f9720d2241f70a42c914ee44b1
Ed <edward.hartnett@noaa.gov>
2023-07-21 07:24:27 -0600

    more testing

--- tests/tst_mrms.c
+++ tests/tst_mrms.c
@@ -13,6 +13,8 @@
 #define MRMS_FILE "data/MRMS_MultiSensor_QPE_24H_Pass2_00.00_20230621-110000.grib2"
 #define LAT_LEN 3500
 #define LON_LEN 7000
+#define LATITUDE "Latitude"
+#define LONGITUDE "Longitude"
 
 int
 main()
@@ -47,15 +49,21 @@ main()
             return G2C_ERROR;
         printf("len0 %ld dimname0 %s\n", len0, dimname0);
         printf("lat[0] = %f lat[%ld] = %f\n", lat[0], len0 - 1, lat[len0 - 1]);
-        /* for (d = 0; d < len0; d++) */
-        /*     printf("lat[%d] = %f\n", d, lat[d]); */
-
-        if (g2c_inq_dim(g2cid, 0, 0, 1, &len1, dimname1, NULL))
+        if (len0 != LAT_LEN || strcmp(dimname0, LATITUDE))
             return G2C_ERROR;
+        if (lat[0] != 54995000 || lat[len0 - 1] != 20005000)
+            return G2C_ERROR;
+
         if (g2c_inq_dim(g2cid, 0, 0, 1, &len1, dimname1, lon))
+            return G2C_ERROR;
+        if (len1 != LON_LEN || strcmp(dimname1, LONGITUDE))
             return G2C_ERROR;
         printf("len1 %ld dimname1 %s\n", len1, dimname1);
         printf("lon[0] = %f lon[%ld] = %f\n", lon[0], len1 - 1, lon[len1 - 1]);
+        if (len1 != LON_LEN || strcmp(dimname1, LONGITUDE))
+            return G2C_ERROR;
+        if (lon[0] != 230004992 || lon[len1 - 1] != 160014992)
+            return G2C_ERROR;
     }
     printf("ok!\n");
     printf("SUCCESS!\n");
