cc0ddd1325a2297ceb8c2c7666996d3e26762150
Edward Hartnett <edward.hartnett@noaa.gov>
2024-01-29 08:26:51 -0700

    more work on index

--- tests/run_index_tests.sh
+++ tests/run_index_tests.sh
@@ -16,7 +16,7 @@ echo "*** Running g2c_index test"
 ../utils/g2c_degrib2 -v gdaswave.t00z.wcoast.0p16.f000.grib2.idx gdaswave.t00z.wcoast.0p16.f000.grib2.idx.degrib2
 
 # Check against expected output.
-#diff -w g2c_degrib2_gdaswave.t00z.wcoast.0p16.f000.grib2.degrib2 data/ref_gdaswave.degrib2.txt
+diff -w g2c_degrib2_gdaswave.t00z.wcoast.0p16.f000.grib2.degrib2 data/ref_gdaswave.degrib2.txt
 
 echo "*** SUCCESS!"
 exit 0
