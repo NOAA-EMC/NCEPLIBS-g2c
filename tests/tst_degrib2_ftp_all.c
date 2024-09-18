c26f12ef0efe8429800ee3ecfc268a90434bbf62
Ed <edward.hartnett@noaa.gov>
2024-09-18 14:42:03 -0600

    debugging code coverage

--- tests/tst_degrib2_ftp_all.c
+++ tests/tst_degrib2_ftp_all.c
@@ -42,7 +42,7 @@ main()
         int f;
         int t;
         int ret;
-	g2c_set_log_level(15);
+	/* g2c_set_log_level(15); */
 
         /* for (f = 0; f < NUM_FILES; f++) */
         for (f = 0; f < 1; f++)
