ee545a0d3450e52f77608b3c788054d6d3a5c21d
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-10-04 10:10:46 -0700

    uncomment some of the specpack tests

--- tests/tst_spec.c
+++ tests/tst_spec.c
@@ -17,33 +17,32 @@ int
 main()
 {
     printf("Testing spec functions.\n");
-    /* printf("Testing specpack()/specunpack() calls..."); */
-    /* { */
-    /*     g2int height = 2, width = 2; */
-    /*     g2int len = PACKED_LEN, ndpts = DATA_LEN; */
-    /*     g2float fld[DATA_LEN] = {1.0, 2.0, 3.0, 0.0}; */
-    /*     g2float fld_in[DATA_LEN]; */
-    /*     unsigned char cpack[PACKED_LEN]; */
-    /*     g2int lcpack = PACKED_LEN; */
-    /*     g2int idrstmpl[10] = {0, 1, 1, 16, 0, 1, 2, 3, 4, 5, 6}; */
-    /*     g2int JJ = 1, KK = 2, MM = 3; */
-    /*     int i; */
+    printf("Testing specpack()/specunpack() calls...");
+    {
+        g2int height = 2, width = 2; */
+        g2int len = PACKED_LEN, ndpts = DATA_LEN; */
+        g2float fld[DATA_LEN] = {1.0, 2.0, 3.0, 0.0}; */
+        g2float fld_in[DATA_LEN]; */
+        unsigned char cpack[PACKED_LEN]; */
+        g2int lcpack = PACKED_LEN; */
+        g2int idrstmpl[10] = {0, 1, 1, 16, 0, 1, 2, 3, 4, 5, 6}; */
+        g2int JJ = 1, KK = 2, MM = 3; */
+        int i; */
 
-    /*     /\* Pack the data. *\/ */
-    /*     specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack); */
+    /* Pack the data. */
+        specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack);
 
-    /*     /\* /\\* Unpack the data. *\\/ *\/ */
-    /*     /\* if (jpcunpack(cpack, len, idrstmpl, ndpts, fld_in)) *\/ */
-    /*     /\*     return G2C_ERROR; *\/ */
+    /* Unpack the data. */
+        if (jpcunpack(cpack, len, idrstmpl, ndpts, fld_in))
+            return G2C_ERROR;
 
-    /*     /\* for (i = 0; i < DATA_LEN; i++) *\/ */
-    /*     /\* { *\/ */
-    /*     /\*     /\\* printf("%g %g\n", fld[i], fld_in[i]); *\\/ *\/ */
-    /*     /\*     if (fld[i] != fld_in[i]) *\/ */
-    /*     /\* 	return G2C_ERROR; *\/ */
-    /*     /\* } *\/ */
-    /* } */
-    /* printf("ok!\n"); */
+        for (i = 0; i < DATA_LEN; i++)
+        {
+            printf("%g %g\n", fld[i], fld_in[i]);
+            if (fld[i] != fld_in[i])
+                return G2C_ERROR;
+    }
+    printf("ok!\n"); 
     /* printf("Testing enc_jpeg2000() call..."); */
     /* { */
     /*     unsigned char data[4] = {1, 2, 3, 4}; */
