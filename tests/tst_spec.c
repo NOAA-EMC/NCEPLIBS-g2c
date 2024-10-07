d4d0f026d70009f3fcd54550d1a6ee4d84e57b16
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-10-07 15:01:35 -0700

    updating fields for tests - still not working

--- tests/tst_spec.c
+++ tests/tst_spec.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DATA_LEN 4
+#define DATA_LEN 6
 #define PACKED_LEN 200
 
 /* Prototypes we are testing. */
@@ -17,23 +17,21 @@ int
 main()
 {
     printf("Testing spec functions.\n");
-    printf("Testing specpack()/specunpack() calls...");
     {
-        g2int height = 2, width = 2;
-        g2int len = PACKED_LEN, ndpts = DATA_LEN;
-        g2float fld[DATA_LEN] = {1.0, 2.0, 3.0, 0.0};
-        g2float fld_in[DATA_LEN];
+        g2int ndpts = DATA_LEN;
+        float fld[DATA_LEN] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+        float fld_in[DATA_LEN];
         unsigned char cpack[PACKED_LEN];
-        g2int lcpack = PACKED_LEN;
-        g2int idrstmpl[10] = {0, 1, 1, 16, 0, 1, 2, 3, 4, 5, 6};
-        g2int JJ = 1, KK = 2, MM = 3; 
+        g2int lcpack;
+        g2int idrstmpl[10] = {0, 1, 1, 16, 0, 0, 0, 0, 2, 1};
+        g2int JJ = 1, KK = 1, MM = 1;
         int i;
 
-    /* Pack the data. */
-        specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack);
+        printf("Packing the data...");
+        specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack); 
 
-    /* Unpack the data. */
-        if (jpcunpack(cpack, len, idrstmpl, ndpts, fld_in))
+        printf("Unpacking the data...");
+        if (specunpack(cpack, idrstmpl, ndpts, JJ, KK, MM, fld_in))
             return G2C_ERROR;
 
         for (i = 0; i < DATA_LEN; i++)
@@ -41,8 +39,35 @@ main()
             printf("%g %g\n", fld[i], fld_in[i]);
             if (fld[i] != fld_in[i])
                 return G2C_ERROR;
+        }
     }
-    printf("ok!\n"); 
+    /* printf("Testing specpack()/specunpack() calls..."); */
+    /* { */
+    /*     g2int height = 2, width = 2; */
+    /*     g2int len = PACKED_LEN, ndpts = DATA_LEN; */
+    /*     g2float fld[DATA_LEN] = {1.0, 2.0, 3.0, 0.0}; */
+    /*     g2float fld_in[DATA_LEN]; */
+    /*     unsigned char cpack[PACKED_LEN]; */
+    /*     g2int lcpack = PACKED_LEN; */
+    /*     g2int idrstmpl[10] = {0, 1, 1, 16, 0, 1, 2, 3, 4, 5, 6}; */
+    /*     g2int JJ = 1, KK = 2, MM = 3; */
+    /*     int i; */
+
+    /*     /\* Pack the data. *\/ */
+    /*     specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, &lcpack); */
+
+    /*     /\* /\\* Unpack the data. *\\/ *\/ */
+    /*     /\* if (jpcunpack(cpack, len, idrstmpl, ndpts, fld_in)) *\/ */
+    /*     /\*     return G2C_ERROR; *\/ */
+
+    /*     /\* for (i = 0; i < DATA_LEN; i++) *\/ */
+    /*     /\* { *\/ */
+    /*     /\*     /\\* printf("%g %g\n", fld[i], fld_in[i]); *\\/ *\/ */
+    /*     /\*     if (fld[i] != fld_in[i]) *\/ */
+    /*     /\* 	return G2C_ERROR; *\/ */
+    /*     /\* } *\/ */
+    /* } */
+    /* printf("ok!\n"); */
     /* printf("Testing enc_jpeg2000() call..."); */
     /* { */
     /*     unsigned char data[4] = {1, 2, 3, 4}; */
