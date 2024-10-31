5e8408bbe941a63d7b3e6fdd5e085512e089a81a
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-10-31 11:29:28 -0700

    fixing some inputs

--- src/decenc_png.c
+++ src/decenc_png.c
@@ -97,7 +97,7 @@ user_flush_data(png_structp png_ptr)
  * @author Stephen Gilbert
  */
 int
-g2c_dec_png(unsigned char *pngbuf, int *width, int *height,
+g2c_dec_png(unsigned char *pngbuf, int width, int height,
         unsigned char *cout)
 {
     g2int width8 = width, height8 = height;
