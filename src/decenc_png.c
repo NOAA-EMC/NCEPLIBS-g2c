186247050f01e72c3c259c3a744f9480972b1154
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-10-31 12:17:19 -0700

    more debug

--- src/decenc_png.c
+++ src/decenc_png.c
@@ -100,9 +100,9 @@ int
 g2c_dec_png(unsigned char *pngbuf, int *width, int *height,
         unsigned char *cout)
 {
-    g2int* width8 = (g2int)&width, height8 = (g2int)&height;
+   // g2int* width8 = (g2int)&width, height8 = (g2int)&height;
 
-    return dec_png(pngbuf, width8, height8, cout);
+    return dec_png(pngbuf, (g2int *)&width, (g2int *)&height, cout);
 }
 
 /**
