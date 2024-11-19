6cf5a1eadaec3077db8308f959755e789e46d935
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-11-19 13:39:33 -0800

    undo changes

--- spack/package.py
+++ spack/package.py
@@ -92,6 +92,5 @@ class G2c(CMakePackage):
         env.set("G2C_INC", join_path(self.prefix, "include"))
 
     def check(self):
-        make("test")
-        #with working_dir(self.builder.build_directory):
-        #    make("test")
+        with working_dir(self.builder.build_directory):
+            make("test")
