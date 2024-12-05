bc459561fca672c0d8e9d6bedf85b2c0c1662c2e
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-05 13:30:05 -0800

    pds 4.93

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -345,6 +345,9 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         4.92: Individual ensemble forecast, control and perturbed, at a horizontal 
         level or in a horizontal layer at a specified local time. */
         {92, 27, 1, {1, 1, 1, 1, 1, 1, -1, -4, 1, -1, -4, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, -4, 1, 1, 4}},
+        /** PDT 4.93 (12/04/2024)
+        4.93: Post-processing analysis or forecast at a horizontal level or in a horizontal layer at a specified local time */
+        {93, 27, 1, {1, 1, 2, 2, 1, 1, 1, 1, 1, -1, -4, 1, -1, -4, 1, 1, 2, 1, 1, 1, 1, 1, 1, -4, 1, 1, 4}},
 };
 
 /**
@@ -1188,6 +1191,23 @@ extpdstemplate(g2int number, g2int *list)
     }
     /* PDT 4.92 (12/04/2024) */
     else if (number == 92)
+    {
+        if (list[15] > 1)
+        {
+            new->extlen = (list[15] - 1) * 11;
+            new->ext = malloc(sizeof(g2int) * new->extlen);
+            for (j = 2; j <= list[15]; j++)
+            {
+                l = (j - 2) * 11;
+                for (k = 0; k < 11; k++)
+                {
+                    new->ext[l + k] = new->map[16 + k];
+                }
+            }
+        }
+    }
+    /* PDT 4.93 (12/04/2024) */
+    else if (number == 93)
     {
         if (list[15] > 1)
         {
