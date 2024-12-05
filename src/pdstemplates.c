77cc9bee14635913d9a91aa22cf0ae13e448510a
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-05 10:33:13 -0800

    pds 4.80

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -298,6 +298,10 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         horizontal layer in a continuous or non-continuous time
         interval for atmospheric chemical constituents with source or sink */
         {79, 34, 1, {1, 1, 2, 1, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 4}},
+        /** PDT 4.80 (12/04/2024)
+        4.80: Analysis or forecast at a horizontal level or in a horizontal layer
+        at a point in time for optical properties of aerosol with source or sink */
+        {80, 27, 0, {1, 1, 2, 1, 1, -1, -4, -1, -4, 1, -1, -4, -1, -4, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4}},
 };
 
 /**
