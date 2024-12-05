6a2a048653b63f7da9ace3821301c25f7b939ff1
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-05 11:48:59 -0800

    pds 4.85

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -320,6 +320,10 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         at a horizontal level or in a horizontal layer in a continuous
         or non-continuous time interval for aerosol with source or sink */
         {84, 39, 1, {1, 1, 2, 1, 1, -1, -4, -1, -4, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 4}},
+        /** PDT 4.85 (12/04/2024)
+        4.85: Individual ensemble forecast, control and perturbed, at a horizontal level
+        or in a horizontal layer in a continuous or non-continuous time interval for aerosol */
+        {85, 38, 1, {1, 1, 2, 1, -1, -4, -1, -4, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 4}},
 };
 
 /**
@@ -1089,6 +1093,23 @@ extpdstemplate(g2int number, g2int *list)
                 for (k = 0; k < 6; k++)
                 {
                     new->ext[l + k] = new->map[33 + k];
+                }
+            }
+        }
+    }
+    /* PDT 4.85 (12/04/2024) */
+    else if (number == 85)
+    {
+        if (list[30] > 1)
+        {
+            new->extlen = (list[30] - 1) * 6;
+            new->ext = malloc(sizeof(g2int) * new->extlen);
+            for (j = 2; j <= list[30]; j++)
+            {
+                l = (j - 2) * 6;
+                for (k = 0; k < 6; k++)
+                {
+                    new->ext[l + k] = new->map[32 + k];
                 }
             }
         }
