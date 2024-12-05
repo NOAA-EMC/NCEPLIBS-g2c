6ff7af5496ad6278a0e7c9b8d77aacce322bfc29
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-05 13:57:27 -0800

    pds 4.95

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -350,8 +350,12 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         {93, 27, 1, {1, 1, 2, 2, 1, 1, 1, 1, 1, -1, -4, 1, -1, -4, 1, 1, 2, 1, 1, 1, 1, 1, 1, -4, 1, 1, 4}},
         /** PDT 4.94 (12/04/2024)
         4.94: Post-processing individual ensemble forecast, control and perturbed, at a horizontal level
-        or in a horizontal layer at a specified local time*/
+        or in a horizontal layer at a specified local time */
         {94, 30, 1, {1, 1, 2, 2, 1, 1, 1, 1, 1, -1, -4, 1, -1, -4, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, -4, 1, 1, 4}},
+        /** PDT 4.95 (12/04/2024)
+        4.95: Average, accumulation, extreme values or other statistically processed value
+        at a horizontal level or in a horizontal layer at a local time */
+        {95, 28, 1, {1, 1, 1, 1, 1, 1, -1, -4, 1, -1, -4, 1, 1, 4, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, -4, 1, 1, 4}},
 };
 
 /**
@@ -1240,6 +1244,23 @@ extpdstemplate(g2int number, g2int *list)
                 for (k = 0; k < 11; k++)
                 {
                     new->ext[l + k] = new->map[19 + k];
+                }
+            }
+        }
+    }
+    /* PDT 4.95 (12/04/2024) */
+    else if (number == 95)
+    {
+        if (list[16] > 1)
+        {
+            new->extlen = (list[16] - 1) * 11;
+            new->ext = malloc(sizeof(g2int) * new->extlen);
+            for (j = 2; j <= list[16]; j++)
+            {
+                l = (j - 2) * 11;
+                for (k = 0; k < 11; k++)
+                {
+                    new->ext[l + k] = new->map[17 + k];
                 }
             }
         }
