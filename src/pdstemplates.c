315348be24104bf9274c57a6b661988c33ee22c4
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-05 09:43:54 -0800

    pds 4.72

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -271,6 +271,11 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         4.71: Post-processing Individual Ensemble Forecast, Control and
         Perturbed, at a horizontal level or in a horizontal layer at a point in time */
         {71, 21, 0, {1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4, 1, 1, 1}},
+        /** PDT 4.72 (12/04/2024)
+        4.72: Post-processing Average, Accumulation, Extreme values
+        or other Statistically processed values at a horizontal level or in
+        a horizontal layer in a continuous or non-continuous time interval */
+        {72, 32, 1, {1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 4}},
 };
 
 /**
@@ -924,6 +929,23 @@ extpdstemplate(g2int number, g2int *list)
         new->ext[k + 27] = 4;
         new->ext[k + 28] = 1;
         new->ext[k + 29] = 4;
+    }
+    /* PDT 4.72 (12/04/2024) */
+    else if (number == 72)
+    {
+        if (list[24] > 1)
+        {
+            new->extlen = (list[24] - 1) * 6;
+            new->ext = malloc(sizeof(g2int) * new->extlen);
+            for (j = 2; j <= list[24]; j++)
+            {
+                l = (j - 2) * 6;
+                for (k = 0; k < 6; k++)
+                {
+                    new->ext[l + k] = new->map[26 + k];
+                }
+            }
+        }
     }
     return new;
 }
