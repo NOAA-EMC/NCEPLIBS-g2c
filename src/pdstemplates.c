060d0c5a68d4dcbb243e85ff1bc8f19e3c16ac52
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-05 10:05:16 -0800

    pds 4.76

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -280,6 +280,10 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         4.73: Post-processing Individual Ensemble Forecast, Control and Perturbed, at a
         horizontal level or in a horizontal layer in a continuous or non-continuous time interval */
         {73, 34, 1, {1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4, 1, 1, 1, 4, 1, 4}},
+        /** PDT 4.76 (12/04/2024)
+        4.76: Analysis or forecast at a horizontal level or in a horizontal layer at a point in
+        time for atmospheric chemical constituents with source or sink */
+        {76, 17, 0, {1, 1, 2, 1, 1, 1, 1, 2, 1, 1, -4, 1, -1, -4, 1, -1, -4}},
 };
 
 /**
