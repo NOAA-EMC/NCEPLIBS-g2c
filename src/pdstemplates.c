21ff896d43e75764e32366db1446e1e260925464
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-06 11:09:16 -0800

    pds 4.116

--- src/pdstemplates.c
+++ src/pdstemplates.c
@@ -410,6 +410,10 @@ static const struct pdstemplate templatespds[G2C_MAX_PDS_TEMPLATE] =
         4.115: Individual ensemble forecast, control and perturbed on generalized tiles at a horizontal level or in a horizontal 
         layer at a point in time */
         {115, 7, 1, {1, 1, 1, 2, 1, 1, 1}},
+        /** PDT 4.116 (12/04/2024)
+        4.116: Individual ensemble forecast, control and perturbed on generalized tiles at a horizontal level or in a horizontal 
+        layer in a continuous or non-continuous time interval */
+        {116, 7, 1, {1, 1, 1, 2, 1, 1, 1}},
 };
 
 /**
@@ -1534,6 +1538,50 @@ extpdstemplate(g2int number, g2int *list)
         new->ext[l + 16] = 1;
         new->ext[l + 17] = 4;
         new->ext[l + 18] = 4;
+    }
+    /* PDT 4.116 (12/04/2024) */
+    else if (number == 116)
+    {
+        new->extlen = list[6] + 33;
+        new->ext = malloc(sizeof(g2int) * new->extlen);
+        for (i = 0; i < list[6]; i++)
+        {
+            new->ext[i] = 1;
+        }
+        l = list[6];
+        new->ext[l] = 1;
+        new->ext[l + 1] = 1;
+        new->ext[l + 2] = 16;
+        new->ext[l + 3] = 1;
+        new->ext[l + 4] = 1;
+        new->ext[l + 5] = 1;
+        new->ext[l + 6] = 2;
+        new->ext[l + 7] = 1;
+        new->ext[l + 8] = 1;
+        new->ext[l + 9] = -4;
+        new->ext[l + 10] = 1;
+        new->ext[l + 11] = -1;
+        new->ext[l + 12] = -4;
+        new->ext[l + 13] = 1;
+        new->ext[l + 14] = -1;
+        new->ext[l + 15] = -4;
+        new->ext[l + 16] = 1;
+        new->ext[l + 17] = 4;
+        new->ext[l + 18] = 4;
+        new->ext[l + 19] = 2;
+        new->ext[l + 20] = 1;
+        new->ext[l + 21] = 1;
+        new->ext[l + 22] = 1;
+        new->ext[l + 23] = 1;
+        new->ext[l + 24] = 1;
+        new->ext[l + 25] = 1;
+        new->ext[l + 26] = 4;
+        new->ext[l + 27] = 1;
+        new->ext[l + 28] = 1;
+        new->ext[l + 29] = 1;
+        new->ext[l + 30] = 4;
+        new->ext[l + 31] = 1;
+        new->ext[l + 32] = 4;
     }
     return new;
 }
