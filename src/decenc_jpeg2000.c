7ae3944f252e43874a28236d57d8d2b578a71b28
Edward Hartnett <edward.hartnett@noaa.gov>
2024-12-27 07:21:02 -0700

    increased jasper memory limit to 100MB

--- src/decenc_jpeg2000.c
+++ src/decenc_jpeg2000.c
@@ -141,11 +141,12 @@ enc_jpeg2000(unsigned char *cin, g2int width, g2int height, g2int nbits,
 
     /* Initialize Jasper. */
 #ifdef JASPER3
+#define HUNDRED_MB 100000000
     jas_conf_clear();
     /* static jas_std_allocator_t allocator; */
     /* jas_std_allocator_init(&allocator); */
     /* jas_conf_set_allocator(JAS_CAST(jas_std_allocator_t *, &allocator)); */
-    jas_conf_set_max_mem_usage(10000000);
+    jas_conf_set_max_mem_usage(HUNDRED_MB);
     jas_conf_set_multithread(true);
     if (jas_init_library())
         return G2_JASPER_INIT;
