f87ba52f15a69209059a711b51f34622e1b9469c
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-03-07 18:00:41 -0800

    address void expression warning

--- src/decenc_openjpeg.c
+++ src/decenc_openjpeg.c
@@ -328,12 +328,12 @@ int_dec_jpeg2000(char *injpc, g2int bufsize, void *outfld, int out_is_g2int)
     if (out_is_g2int)
     {
         for (unsigned int i = 0; i < image->comps[0].w * image->comps[0].h; i++)
-            (g2int *)outfld[i] = (g2int)(image->comps[0].data[i] & mask);
+            ((g2int *)outfld)[i] = (g2int)(image->comps[0].data[i] & mask);
     }
     else
     {
         for (unsigned int i = 0; i < image->comps[0].w * image->comps[0].h; i++)
-            (int *)outfld[i] = (int)(image->comps[0].data[i] & mask);
+            ((int *)outfld)[i] = (int)(image->comps[0].data[i] & mask);
     }
 
     if (!opj_end_decompress(codec, stream))
