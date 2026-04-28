7c11bb8cece90a7b235df26ea79931ca2cccd0a8
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-04-28 11:25:39 -0700

    move spack updates to another PR

--- spack/package.py
+++ spack/package.py
@@ -4,7 +4,6 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
-from spack_repo.builtin.build_systems.cmake import CMakePackage
 
 class G2c(CMakePackage):
     """This library contains C decoder/encoder routines for GRIB edition 2.
