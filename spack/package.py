00fdcc1a1d5365e56b14c4c0c77af0478a7a1263
Hang-Lei-NOAA <44901908+Hang-Lei-NOAA@users.noreply.github.com>
2026-08-27 10:17:34 -0400

    Update patch method to comment out find_package line

--- spack/package.py
+++ spack/package.py
@@ -91,7 +91,7 @@ class G2c(CMakePackage):
         env.set("G2C_INC", join_path(self.prefix, "include"))
 
     def patch(self):
-        filter_file(r"^(\s+find_package\(PkgConfig REQUIRED\))", r"#\1", "CMakeLists.txt")
+        filter_file(r"find_package\(PkgConfig REQUIRED\)", r"#find_package(PkgConfig REQUIRED)", "CMakeLists.txt")
 
     def check(self):
         with working_dir(self.build_directory):
